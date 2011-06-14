6e368829c3658ff704ef7ff8289a4de3d2adb994
Thomas Gleixner <tglx@linutronix.de>
2011-06-14 17:05:09 +0200

    fs-block-rt-support.patch
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- fs/file.c
+++ fs/file.c
@@ -105,14 +105,14 @@ void free_fdtable_rcu(struct rcu_head *rcu)
 		kfree(fdt->open_fds);
 		kfree(fdt);
 	} else {
-		fddef = &get_cpu_var(fdtable_defer_list);
+		fddef = &per_cpu(fdtable_defer_list, get_cpu_light());
 		spin_lock(&fddef->lock);
 		fdt->next = fddef->next;
 		fddef->next = fdt;
 		/* vmallocs are handled from the workqueue context */
 		schedule_work(&fddef->wq);
 		spin_unlock(&fddef->lock);
-		put_cpu_var(fdtable_defer_list);
+		put_cpu_light();
 	}
 }
 
