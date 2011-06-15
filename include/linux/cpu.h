de50df7a56f5b3af44e0ed65f6c786c1935bb267
Thomas Gleixner <tglx@linutronix.de>
2011-06-15 12:36:06 +0200

    hotplug: Lightweight get online cpus
    get_online_cpus() is a heavy weight function which involves a global
    mutex. migrate_disable() wants a simpler construct which prevents only
    a CPU from going doing while a task is in a migrate disabled section.
    
    Implement a per cpu lockless mechanism, which serializes only in the
    real unplug case on a global mutex. That serialization affects only
    tasks on the cpu which should be brought down.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/cpu.h
+++ include/linux/cpu.h
@@ -167,6 +167,8 @@ extern struct sysdev_class cpu_sysdev_class;
 
 extern void get_online_cpus(void);
 extern void put_online_cpus(void);
+extern void pin_current_cpu(void);
+extern void unpin_current_cpu(void);
 #define hotcpu_notifier(fn, pri)	cpu_notifier(fn, pri)
 #define register_hotcpu_notifier(nb)	register_cpu_notifier(nb)
 #define unregister_hotcpu_notifier(nb)	unregister_cpu_notifier(nb)
@@ -189,6 +191,8 @@ static inline void cpu_hotplug_driver_unlock(void)
 
 #define get_online_cpus()	do { } while (0)
 #define put_online_cpus()	do { } while (0)
+static inline void pin_current_cpu(void) { }
+static inline void unpin_current_cpu(void) { }
 #define hotcpu_notifier(fn, pri)	do { (void)(fn); } while (0)
 /* These aren't inline functions due to a GCC bug. */
 #define register_hotcpu_notifier(nb)	({ (void)(nb); 0; })
