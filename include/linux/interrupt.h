679d4639a9ba22135838d50ab9fdcb8df5eb0a99
Thomas Gleixner <tglx@linutronix.de>
2011-06-28 15:44:15 +0200

    softirq-thread-do-softirq.patch
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -443,6 +443,7 @@ struct softirq_action
 
 asmlinkage void do_softirq(void);
 asmlinkage void __do_softirq(void);
+static inline void thread_do_softirq(void) { do_softirq(); }
 extern void open_softirq(int nr, void (*action)(struct softirq_action *));
 extern void softirq_init(void);
 extern void __raise_softirq_irqoff(unsigned int nr);
--- net/core/dev.c
+++ net/core/dev.c
@@ -3204,7 +3204,7 @@ int netif_rx_ni(struct sk_buff *skb)
 	preempt_disable();
 	err = netif_rx(skb);
 	if (local_softirq_pending())
-		do_softirq();
+		thread_do_softirq();
 	preempt_enable();
 
 	return err;
