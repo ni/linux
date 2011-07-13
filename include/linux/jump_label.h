946be5027dd3a25d8844ae11edef0e0ec73b2029
Thomas Gleixner <tglx@linutronix.de>
2011-07-13 11:03:16 +0200

    jump-label-rt.patch
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/jump_label.h
+++ include/linux/jump_label.h
@@ -4,7 +4,7 @@
 #include <linux/types.h>
 #include <linux/compiler.h>
 
-#if defined(CC_HAVE_ASM_GOTO) && defined(CONFIG_JUMP_LABEL)
+#if defined(CC_HAVE_ASM_GOTO) && defined(CONFIG_JUMP_LABEL) && !defined(CONFIG_PREEMPT_BASE)
 
 struct jump_label_key {
 	atomic_t enabled;
