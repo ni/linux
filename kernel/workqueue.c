35714c9d7a031cb89951d31875d00aa4d500a10f
Steven Rostedt <srostedt@redhat.com>
2012-06-06 15:00:13 -0400

    Revert "workqueue: skip nr_running sanity check in worker_enter_idle() if trustee is active"
    This reverts commit 5d79c6f64a904afc92a329f80abe693e3ae105fe.
    
    Reported-by: Ibrahim Umar <iambaim@gmail.com>
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -1213,13 +1213,8 @@ static void worker_enter_idle(struct worker *worker)
 				jiffies + IDLE_WORKER_TIMEOUT);
 	}
 
-	/*
-	 * Sanity check nr_running.  Because trustee releases gcwq->lock
-	 * between setting %WORKER_ROGUE and zapping nr_running, the
-	 * warning may trigger spuriously.  Check iff trustee is idle.
-	 */
-	WARN_ON_ONCE(gcwq->trustee_state == TRUSTEE_DONE &&
-		     gcwq->nr_workers == gcwq->nr_idle &&
+	/* sanity check nr_running */
+	WARN_ON_ONCE(gcwq->nr_workers == gcwq->nr_idle &&
 		     atomic_read(get_gcwq_nr_running(gcwq->cpu)));
 }
 
