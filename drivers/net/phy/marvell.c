ac34f8edf20b266373a1882a638dc8af7c98022c
Jeff Westfahl <jeff.westfahl@ni.com>
2012-07-11 22:14:17 +0200

    Marvell 88E151x PHY: Configure interrupt output
    By default, the interrupt output of the Marvell 88E1512 PHY is
    configured as an LED output, and is driven low. If we have a case where
    eth1 is brought up before eth0, this causes an interrupt storm, and
    Linux eventually gives up and disables the PHY interrupt line. Adding a
    probe function to the Marvell 88E1512 PHY driver and moving the
    interrupt output configuration there resolves this problem.
    
    [bm: Adjust for d2fa47d9dd5c5f82d78c4503cf33989f67e8aa64 'phy: marvell: Add ethtool statistics counters']
    
    Signed-off-by: Jeff Westfahl <jeff.westfahl@ni.com>
    Signed-off-by: Mihaly Varga <mihaly.varga@ni.com>
    Acked-by: Brad Mouring <brad.mouring@ni.com>
    Acked-by: Ben Shelton <ben.shelton@ni.com>
    Acked-by: Jaeden Amero <jaeden.amero@ni.com>
    Natinst-ReviewBoard-ID: 81627
    
    [bm: Restore correct page on LED/irq config]
    Signed-off-by: Brad Mouring <brad.mouring@ni.com>
    Acked-by: Jeff Westfahl <jeff.westfahl@ni.com>
    Natinst-ReviewBoard-ID: 148255
    
    [gratian: split LED pin setup from m88e151x_probe; fixes conflict with 0b04680fdae4 ("phy: marvell: Add support for temperature sensor")]
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>
    [cvadrevu: Fixed trivial conflict with b697d9d38a5a5 ("net: phy: marvell: add SFP support for 88E1510")]
    [cvadrevu: Updated marvell_get_page to marvell_read_page due to 424ca4c55121 ("net: phy: marvell: fix paged access races")]
    Signed-off-by: Chaitanya Vadrevu <chaitanya.vadrevu@emerson.com>

--- drivers/net/phy/marvell.c
+++ drivers/net/phy/marvell.c
@@ -181,6 +181,8 @@
 #define MII_88E3016_PHY_SPEC_CTRL	0x10
 #define MII_88E3016_DISABLE_SCRAMBLER	0x0200
 #define MII_88E3016_AUTO_MDIX_CROSSOVER	0x0030
+#define MII_88E1512_LEDTimerControl_InterruptEnable 0x0080
+
 
 #define MII_88E1510_GEN_CTRL_REG_1		0x14
 #define MII_88E1510_GEN_CTRL_REG_1_MODE_MASK	0x7
@@ -3682,9 +3684,44 @@ static const struct sfp_upstream_ops m88e1510_sfp_ops = {
 	.disconnect_phy = phy_sfp_disconnect_phy,
 };
 
+static int m88e151x_setup_interrupt_pin(struct phy_device *phydev)
+{
+	u32 led_timer_control;
+	int err, old_page;
+
+	old_page = marvell_read_page(phydev);
+	if (old_page < 0)
+		return old_page;
+
+	/* Enable the interrupt output. This is just a pin configuration,
+	 * we're not actually enabling any interrupts here. But the default
+	 * configuration causes our interrupt pin to be asserted. We need
+	 * to stop that as early as possible.
+	 */
+	err = marvell_set_page(phydev, MII_MARVELL_LED_PAGE);
+
+	if (err < 0)
+		return err;
+
+	led_timer_control = phy_read(phydev, MII_88E1318S_PHY_LED_TCR);
+	led_timer_control |= MII_88E1512_LEDTimerControl_InterruptEnable;
+
+	err = phy_write(phydev, MII_88E1318S_PHY_LED_TCR, led_timer_control);
+	if (err < 0)
+		return err;
+
+	err = marvell_set_page(phydev, old_page);
+
+	return err;
+}
+
 static int m88e1510_probe(struct phy_device *phydev)
 {
 	int err;
+
+	err = m88e151x_setup_interrupt_pin(phydev);
+	if (err)
+		return err;
 
 	err = marvell_probe(phydev);
 	if (err)
