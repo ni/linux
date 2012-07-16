d2bfcee1ec0d76b3b61be6ed17c227dc492d498e
Steven Rostedt <srostedt@redhat.com>
2012-07-16 08:07:44 +0000

    workqueue: Revert workqueue: Fix cpuhotplug trainwreck
    Revert
    
        Author: Peter Zijlstra <a.p.zijlstra@chello.nl>
        Date:   Fri Sep 30 11:57:58 2011 +0200
        workqueue: Fix cpuhotplug trainwreck
    
    As TREAD_BOUND no longer affects cpu down, and this code introduced
    a lot of races with taking down a CPU.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/cpu.h
+++ include/linux/cpu.h
@@ -66,10 +66,8 @@ enum {
 	/* migration should happen before other stuff but after perf */
 	CPU_PRI_PERF		= 20,
 	CPU_PRI_MIGRATION	= 10,
-
-	CPU_PRI_WORKQUEUE_ACTIVE	= 5,  /* prepare workqueues for others */
-	CPU_PRI_NORMAL			= 0,
-	CPU_PRI_WORKQUEUE_INACTIVE	= -5, /* flush workqueues after others */
+	/* prepare workqueues for other notifiers */
+	CPU_PRI_WORKQUEUE	= 5,
 };
 
 #define CPU_ONLINE		0x0002 /* CPU (unsigned)v is up */
