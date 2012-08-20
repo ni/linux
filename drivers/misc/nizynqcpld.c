ef40de560d7bf67a9a2ce95d4e7c1241e3db18be
Josh Cartwright <josh.cartwright@ni.com>
2012-08-20 18:43:12 -0500

    nizynqcpld: Add 'bootmode' attribute
    This is a read/write attribute that will set the necessary bits in the
    scratch registers used to determine which mode the board boots into.
    Currently, valid modes are 'runtime', 'safemode', and 'recovery'.
    
    Signed-off-by: Josh Cartwright <josh.cartwright@ni.com>

--- drivers/misc/nizynqcpld.c
+++ drivers/misc/nizynqcpld.c
@@ -184,13 +184,81 @@ static DEVICE_ATTR(scratch_softreset, S_IRUSR|S_IWUSR,
 static DEVICE_ATTR(scratch_hardreset, S_IRUSR|S_IWUSR,
 		nizynqcpld_scratchhr_show, nizynqcpld_scratchhr_store);
 
+static const char * const bootmode_strings[] = {
+	"runtime", "safemode", "recovery",
+};
+
+static ssize_t nizynqcpld_bootmode_show(struct device *dev,
+					struct device_attribute *attr,
+					char *buf)
+{
+	struct nizynqcpld *cpld = dev_get_drvdata(dev);
+	struct nizynqcpld_desc *desc = cpld->desc;
+	int err;
+	u8 tmp;
+
+	nizynqcpld_lock(cpld);
+	err = nizynqcpld_read(cpld, desc->scratch_hr_addr, &tmp);
+	nizynqcpld_unlock(cpld);
+
+	if (err)
+		return err;
+
+	tmp &= 0x3;
+	if (tmp >= ARRAY_SIZE(bootmode_strings))
+		return -EINVAL;
+
+	return sprintf(buf, "%s\n", bootmode_strings[tmp]);
+}
+
+static int nizynqcpld_set_bootmode(struct nizynqcpld *cpld, u8 mode)
+{
+	struct nizynqcpld_desc *desc = cpld->desc;
+	int err;
+	u8 tmp;
+
+	nizynqcpld_lock(cpld);
+
+	err = nizynqcpld_read(cpld, desc->scratch_hr_addr, &tmp);
+	if (err)
+		goto unlock_out;
+
+	tmp &= ~0x3;
+	tmp |= mode;
+
+	err = nizynqcpld_write(cpld, desc->scratch_hr_addr, tmp);
+
+unlock_out:
+	nizynqcpld_unlock(cpld);
+	return err;
+}
+
+static ssize_t nizynqcpld_bootmode_store(struct device *dev,
+					 struct device_attribute *attr,
+					 const char *buf, size_t count)
+{
+	struct nizynqcpld *cpld = dev_get_drvdata(dev);
+	u8 i;
+
+	for (i = 0; i < ARRAY_SIZE(bootmode_strings); i++)
+		if (!strcmp(buf, bootmode_strings[i]))
+			return nizynqcpld_set_bootmode(cpld, i) ?: count;
+
+	return -EINVAL;
+}
+
+static DEVICE_ATTR(bootmode, S_IRUSR|S_IWUSR, nizynqcpld_bootmode_show,
+		   nizynqcpld_bootmode_store);
+
 static const struct attribute *nizynqcpld_attrs[] = {
+	&dev_attr_bootmode.attr,
 	&dev_attr_scratch_softreset.attr,
 	&dev_attr_scratch_hardreset.attr,
 	NULL
 };
 
 static const struct attribute *dosequis6_attrs[] = {
+	&dev_attr_bootmode.attr,
 	&dev_attr_scratch_softreset.attr,
 	&dev_attr_scratch_hardreset.attr,
 	NULL
