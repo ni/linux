2ef05b4e668cf2eff72e05de50a0c4403c5d1a94
Kyle Teske <kyle.teske@ni.com>
2012-08-28 12:48:18 -0500

    fpgaperipheral: Add driver to notify clients on FPGA downloads
    Clients are drivers for peripherals with physical lines routed through
    the FPGA.  Ethernet and serial will want to receive notifications so
    they can avoid accessing hardware while the FPGA is unprogrammed.
    
    Signed-off-by: Kyle Teske <kyle.teske@ni.com>
    Natinst-ReviewBoard-ID: 26536

---
+++ include/misc/fpgaperipheral.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2012 National Instruments Corp.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _FPGA_PERIPHERAL_H_
+#define _FPGA_PERIPHERAL_H_
+
+#include <linux/notifier.h>
+
+/*
+ * The value passed to notifier callbacks will be one of these.
+ * FPGA_PERIPHERAL_DOWN: The FPGA is about to be unprogrammed.
+ * FPGA_PERIPHERAL_UP: The FPGA is now programmed.
+ *  This may be called even if not preceded by an FPGA_PERIPHERAL_DOWN.
+ *
+ * Client callbacks should always return notifier_from_errno(0) so the
+ * call chain continues to all clients.
+ */
+#define FPGA_PERIPHERAL_DOWN	0
+#define FPGA_PERIPHERAL_UP	1
+
+extern struct blocking_notifier_head fpgaperipheral_notifier_list;
+
+#endif /* _FPGA_PERIPHERAL_H_ */
