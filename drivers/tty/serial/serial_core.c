dcf0b05acb5d06457730550c471aa643c9477029
Jaeden Amero <jaeden.amero@ni.com>
2012-09-10 15:04:47 -0500

    8250: Handle FPGA reprogramming
    The 8250 driver will now subscribe to and handle fpgaperipheral
    notifications. When the FPGA disappears, the port be suspended, but
    not removed from the system. When the FPGA reappears, the port will be
    resumed.
    
    Signed-off-by: Jaeden Amero <jaeden.amero@ni.com>
    (gratian: squashed with "8250: Replace 'FGPA' with 'FPGA'")
    Signed-off-by: Xander Huff <xander.huff@ni.com>
    Signed-off-by: Brad Mouring <brad.mouring@ni.com>
    Natinst-ReviewBoard-ID: 156851
    (cherry picked from commit 89c5032002965d3efdf2812eb2f81541698da001)
    [gratian: fix conflicts with 4047b37122d1 ("serial: core: Prevent unsafe uart port access, part 1")]
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>

--- drivers/tty/serial/serial_core.c
+++ drivers/tty/serial/serial_core.c
@@ -133,7 +133,7 @@ static void __uart_start(struct tty_struct *tty)
 	struct uart_state *state = tty->driver_data;
 	struct uart_port *port = state->uart_port;
 
-	if (port && !uart_tx_stopped(port))
+	if (port && !uart_tx_stopped(port) && !port->suspended)
 		port->ops->start_tx(port);
 }
 
@@ -219,6 +219,14 @@ static int uart_port_startup(struct tty_struct *tty, struct uart_state *state,
 		uart_circ_clear(&state->xmit);
 	}
 
+#ifdef CONFIG_FPGA_PERIPHERAL
+	down_read(&uport->fpga_lock);
+	if (uport->fpga_state != FPGA_UP) {
+		up_read(&uport->fpga_lock);
+		return -ENODEV;
+	}
+#endif
+
 	retval = uport->ops->startup(uport);
 	if (retval == 0) {
 		if (uart_console(uport) && uport->cons->cflag) {
@@ -241,6 +249,10 @@ static int uart_port_startup(struct tty_struct *tty, struct uart_state *state,
 		if (init_hw && C_BAUD(tty))
 			uart_port_dtr_rts(uport, 1);
 	}
+
+#ifdef CONFIG_FPGA_PERIPHERAL
+	up_read(&uport->fpga_lock);
+#endif
 
 	/*
 	 * This is to allow setserial on this port. People may want to set
@@ -1063,12 +1075,25 @@ static int uart_tiocmget(struct tty_struct *tty)
 	if (!uport)
 		goto out;
 
+#ifdef CONFIG_FPGA_PERIPHERAL
+	down_read(&uport->fpga_lock);
+	if (uport->fpga_state != FPGA_UP) {
+		result = -ENODEV;
+		goto out_up;
+	}
+#endif
+
 	if (!tty_io_error(tty)) {
 		result = uport->mctrl;
 		spin_lock_irq(&uport->lock);
 		result |= uport->ops->get_mctrl(uport);
 		spin_unlock_irq(&uport->lock);
 	}
+
+#ifdef CONFIG_FPGA_PERIPHERAL
+out_up:
+	up_read(&uport->fpga_lock);
+#endif
 out:
 	mutex_unlock(&port->mutex);
 	return result;
@@ -1087,10 +1112,23 @@ uart_tiocmset(struct tty_struct *tty, unsigned int set, unsigned int clear)
 	if (!uport)
 		goto out;
 
+#ifdef CONFIG_FPGA_PERIPHERAL
+	down_read(&uport->fpga_lock);
+	if (uport->fpga_state != FPGA_UP) {
+		ret = -ENODEV;
+		goto out_up;
+	}
+#endif
+
 	if (!tty_io_error(tty)) {
 		uart_update_mctrl(uport, set, clear);
 		ret = 0;
 	}
+
+#ifdef CONFIG_FPGA_PERIPHERAL
+out_up:
+	up_read(&uport->fpga_lock);
+#endif
 out:
 	mutex_unlock(&port->mutex);
 	return ret;
@@ -1368,6 +1406,18 @@ uart_ioctl(struct tty_struct *tty, unsigned int cmd, unsigned long arg)
 	/*
 	 * The following should only be used when hardware is present.
 	 */
+	mutex_lock(&port->mutex);
+	uport = uart_port_check(state);
+
+#ifdef CONFIG_FPGA_PERIPHERAL
+	down_read(&uport->fpga_lock);
+	if (uport->fpga_state != FPGA_UP) {
+		up_read(&uport->fpga_lock);
+		ret = -ENODEV;
+		goto out_up;
+	}
+#endif
+
 	switch (cmd) {
 	case TIOCMIWAIT:
 		ret = uart_wait_modem_status(state, arg);
@@ -1375,10 +1425,7 @@ uart_ioctl(struct tty_struct *tty, unsigned int cmd, unsigned long arg)
 	}
 
 	if (ret != -ENOIOCTLCMD)
-		goto out;
-
-	mutex_lock(&port->mutex);
-	uport = uart_port_check(state);
+		goto out_up;
 
 	if (!uport || tty_io_error(tty)) {
 		ret = -EIO;
@@ -1408,6 +1455,9 @@ uart_ioctl(struct tty_struct *tty, unsigned int cmd, unsigned long arg)
 		break;
 	}
 out_up:
+#ifdef CONFIG_FPGA_PERIPHERAL
+	up_read(&uport->fpga_lock);
+#endif
 	mutex_unlock(&port->mutex);
 out:
 	return ret;
@@ -1464,6 +1514,18 @@ static void uart_set_termios(struct tty_struct *tty,
 		goto out;
 	}
 
+#ifdef CONFIG_FPGA_PERIPHERAL
+	down_read(&uport->fpga_lock);
+	/*
+	 * If the FPGA state is not FPGA_UP (the re-programming failed),
+	 * we cannot return an error code from this function.
+	 */
+	if (uport->fpga_state != FPGA_UP) {
+		up_read(&uport->fpga_lock);
+		goto out;
+	}
+#endif
+
 	uart_change_speed(tty, state, old_termios);
 	/* reload cflag from termios; port driver may have overriden flags */
 	cflag = tty->termios.c_cflag;
@@ -1478,6 +1540,11 @@ static void uart_set_termios(struct tty_struct *tty,
 			mask |= TIOCM_RTS;
 		uart_set_mctrl(uport, mask);
 	}
+
+#ifdef CONFIG_FPGA_PERIPHERAL
+	up_read(&uport->fpga_lock);
+#endif
+
 out:
 	mutex_unlock(&state->port.mutex);
 }
@@ -1590,7 +1657,7 @@ static void uart_wait_until_sent(struct tty_struct *tty, int timeout)
 	 * 'timeout' / 'expire' give us the maximum amount of time
 	 * we wait.
 	 */
-	while (!port->ops->tx_empty(port)) {
+	while (port->suspended || !port->ops->tx_empty(port)) {
 		msleep_interruptible(jiffies_to_msecs(char_time));
 		if (signal_pending(current))
 			break;
