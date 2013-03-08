1e4fb6a642651a83cd1abfa831c91d406231620a
Nathan Sullivan <nathan.sullivan@ni.com>
2013-03-08 15:17:12 -0600

    LCI UIO: Enable interrupt support
    Using uio_pdrv_genirq as an example, add support for our FPGA interrupt
    to the UIO driver.  Also, enable it in the device tree for LCI.
    
    Signed-off-by: Nathan Sullivan <nathan.sullivan@ni.com>
    Acked-by: Joe Hershberger <joe.hershberger@ni.com>

--- drivers/uio/uio_lci.c
+++ drivers/uio/uio_lci.c
@@ -1,5 +1,5 @@
-//UIO driver for LCI.  Allows mmap-ing just the FPGA registers, handling interrupts from the FPGA,
-//and getting page lists for userspace DMA buffers through sysfs
+//UIO driver for LCI.  Allows mmap-ing just the FPGA registers amd DMA space,
+//and handling interrupts from the FPGA
 
 #include <linux/device.h>
 #include <linux/module.h>
@@ -12,12 +12,44 @@
 
 struct lci_dev {
 	struct uio_info info;
+	unsigned long flags;
+	spinlock_t lock;
 };
+
+#define IRQ_ENABLE_FLAG 0
+
+static irqreturn_t uio_lci_handler(int irq, struct uio_info *dev_info)
+{
+	struct lci_dev *priv = dev_info->priv;
+
+	if (!test_and_set_bit(IRQ_ENABLE_FLAG, &priv->flags))
+		disable_irq_nosync(irq);
+
+	return IRQ_HANDLED;
+}
+
+static int uio_lci_irqcontrol(struct uio_info *dev_info, s32 irq_on)
+{
+	struct lci_dev *priv = dev_info->priv;
+	unsigned long flags;
+
+	spin_lock_irqsave(&priv->lock, flags);
+	if(irq_on) {
+		if (test_and_clear_bit(IRQ_ENABLE_FLAG, &priv->flags))
+			enable_irq(dev_info->irq);
+	} else {
+		if (!test_and_set_bit(IRQ_ENABLE_FLAG, &priv->flags))
+			disable_irq(dev_info->irq);
+	}
+	spin_unlock_irqrestore(&priv->lock, flags);
+
+	return 0;
+}
 
 static int __devinit lci_probe(struct platform_device *dev)
 {
 	struct lci_dev* pdev;
-	int err;
+	int irq, err;
 
 	printk(KERN_INFO "Probed the lci device!\n");
 	//register with uio
@@ -29,6 +61,9 @@ static int __devinit lci_probe(struct platform_device *dev)
 
 	pdev->info.name = "lci_dev";
 	pdev->info.version = "1.00a";
+	pdev->info.handler = uio_lci_handler;
+	pdev->info.irqcontrol = uio_lci_irqcontrol;
+	pdev->info.priv = pdev;
 	//Mem region 0 is for the FPGA registers
 	pdev->info.mem[0].name = "registers";
 	pdev->info.mem[0].addr = dev->resource[0].start;
@@ -40,6 +75,16 @@ static int __devinit lci_probe(struct platform_device *dev)
 	pdev->info.mem[1].addr = dev->resource[1].start;
 	pdev->info.mem[1].size = dev->resource[1].end - dev->resource[1].start + 1;
 	pdev->info.mem[1].memtype = UIO_MEM_PHYS;
+	//Set flags to 0, since our IRQ is enabled initially
+	pdev->flags = 0;
+
+	irq = platform_get_irq(dev, 0);
+	if (irq == -ENXIO)
+		pdev->info.irq = UIO_IRQ_NONE;
+	else
+		pdev->info.irq = irq;
+
+	spin_lock_init(&pdev->lock);
 
 	if(uio_register_device(&dev->dev, &pdev->info))
 		goto err_register;
