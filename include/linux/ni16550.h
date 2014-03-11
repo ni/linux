a89b4303cf01d685703c015b9a549947a07b4416
Jaeden Amero <jaeden.amero@ni.com>
2014-03-11 16:52:15 -0500

    8250: Add a driver for NI 16550 UART
    The National Instruments (NI) 16550 is a standard 16550 with larger
    FIFOs and embedded RS-485 transceiver control circuitry. This patch
    adds a driver that can operate this UART.
    
    Signed-off-by: Jaeden Amero <jaeden.amero@ni.com>
    Acked-by: Karthik Manamcheri <karthik.manamcheri@ni.com>
    [gratian: fix conflict with aa42db44f670 ("8250: of: remove remnants of generic of_serial driver")]
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>
    [bstreiff: renumber out-of-tree definition of PORT_NI16550 from 118 to 123.]
    Signed-off-by: Brandon Streiff <brandon.streiff@ni.com>

---
+++ include/linux/ni16550.h
@@ -0,0 +1,26 @@
+/*
+ *  NI 16550 Transceiver Driver
+ *
+ *  The National Instruments (NI) 16550 has built-in RS-485 transceiver control
+ *  circuitry. This driver provides the transceiver control functionality
+ *  for the RS-485 ports and uses the 8250 driver for the UART functionality.
+ *
+ *  Copyright 2012 National Instruments Corporation
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ *  more details.
+ */
+
+#ifndef _NI16550_H
+#define _NI16550_H
+
+void ni16550_port_setup(struct uart_port *port);
+
+#endif
