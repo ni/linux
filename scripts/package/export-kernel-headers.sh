52b11bac7a339b822d2fe48c7d12c2d8c834d412
Brad Mouring <brad.mouring@ni.com>
2014-03-26 14:25:33 -0500

    builditb: Generate needed files for NI Zynq boards
    This change will generate the itb, headers squashfs, and modules folder
    to enable you to boot a new kernel on an NI Zynq-based controller.
    
    Signed-off-by: Brad Mouring <brad.mouring@ni.com>

---
+++ scripts/package/export-kernel-headers.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+# This script largely borrowed from rpmspec for the fedora kernel packages (specifically the -devel section)
+fail() { echo "$@"; exit 1; }
+
+SOURCEDIR=`cd $1; pwd`
+OBJDIR=`cd $2; pwd`
+mkdir -p "$3"
+OUTDIR=`cd $3; pwd`
+ARCH=$4
+
+[ -d "$SOURCEDIR" -a -d "$OBJDIR" ] || fail "Usage $0 <kernel source dir> <kernel obj dir> <header export dir>"
+
+[ -e "$SOURCEDIR"/Makefile -a -e "$SOURCEDIR"/Kconfig ] || fail "Must run from within kernel tree."
+
+# Needed for copies that use --parents
+cd "$SOURCEDIR"
+cp --parents $(find . -type f -name "Makefile*" -o -name "Kconfig*") "$OUTDIR"
+rm -rf "$OUTDIR"/{Documentation,scripts,include}
+cp -a "$SOURCEDIR"/scripts "$OUTDIR"
+cp -a "$OBJDIR"/scripts/* "$OUTDIR"/scripts
+rm -f "$OUTDIR"/scripts/*.o "$OUTDIR"/scripts/.*.cmd
+rm -f "$OUTDIR"/scripts/*/*.o "$OUTDIR"/scripts/*/.*.cmd
+rm -rf "$OUTDIR"/.pc
+cp -a --parents arch/"$ARCH"/include "$OUTDIR"
+cp -a "$OBJDIR"/arch/"$ARCH"/include/generated "$OUTDIR"/arch/"$ARCH"/include
+if [ -d arch/"$ARCH"/mach-zynq ]
+then
+	cp -a --parents arch/"$ARCH"/mach-zynq/include "$OUTDIR"
+fi
+cp -a "$SOURCEDIR"/include "$OUTDIR"/include
+cp -a "$OBJDIR"/include/* "$OUTDIR"/include
+cp "$OBJDIR"/.config "$OUTDIR"/.config
+touch -r "$OUTDIR"/Makefile "$OUTDIR"/include/linux/version.h
+touch -r "$OUTDIR"/.config "$OUTDIR"/include/linux/autoconf.h
