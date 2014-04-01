dcd249848dbb1ca249f97fdc88042d049e53284c
Ben Shelton <ben.shelton@ni.com>
2014-04-01 11:31:31 -0500

    shared: Adding mcopy syscall for Zynq and x64
    Signed-off-by: Ben Shelton <ben.shelton@ni.com>
    Acked-by: Scot Salmon <scot.salmon@ni.com>
    Acked-by: Terry Wilcox <terry.wilcox@ni.com>
    Natinst-ReviewBoard-ID: 69848
    [gratian: convert to new syscall table format for arm]
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>
    [bstreiff: update the number for this painful out-of-tree syscall]
    Signed-off-by: Brandon Streiff <brandon.streiff@ni.com>
    [gratian: update due to new syscall introduced by ecb8ac8b1f14
     ("mm/madvise: introduce process_madvise() syscall: an external memory hinting AP")]
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -860,8 +860,11 @@ __SYSCALL(__NR_faccessat2, sys_faccessat2)
 #define __NR_process_madvise 440
 __SYSCALL(__NR_process_madvise, sys_process_madvise)
 
+#define __NR_mcopy 441
+__SYSCALL(__NR_mcopy, sys_mcopy)
+
 #undef __NR_syscalls
-#define __NR_syscalls 441
+#define __NR_syscalls 442
 
 /*
  * 32 bit systems traditionally used different
