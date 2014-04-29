66046fdb76671bffd3c17027f2c76fcfc179008a
Jeff Westfahl <jeff.westfahl@ni.com>
2014-04-29 09:35:46 -0500

    i2c: cadence: Support reading the bus number from the device tree
    When multiple I2C busses are present, we need a way to deterministically
    specify the bus number for each bus. Here we add support for reading a
    bus-id field from the device tree. We must use i2c_add_numbered_adapter
    when specifying a bus number. We use a default value of -1 if the bus
    number isn't specified in the device tree, which gives the same behavior
    as using i2c_add_adapter.
    
    Signed-off-by: Jeff Westfahl <jeff.westfahl@ni.com>

--- drivers/i2c/busses/i2c-cadence.c
+++ drivers/i2c/busses/i2c-cadence.c
@@ -844,6 +844,14 @@ static int cdns_i2c_probe(struct platform_device *pdev)
 
 	id->irq = platform_get_irq(pdev, 0);
 
+	/*
+	 * A bus number of -1 means dynamically assign, default to that if
+	 * bus-id isn't specified in the device tree.
+	 */
+	ret = of_property_read_u32(pdev->dev.of_node, "bus-id", &id->adap.nr);
+	if (ret)
+		id->adap.nr = -1;
+
 	id->adap.dev.of_node = pdev->dev.of_node;
 	id->adap.algo = &cdns_i2c_algo;
 	id->adap.timeout = CDNS_I2C_TIMEOUT;
@@ -891,7 +899,7 @@ static int cdns_i2c_probe(struct platform_device *pdev)
 		goto err_clk_dis;
 	}
 
-	ret = i2c_add_adapter(&id->adap);
+	ret = i2c_add_numbered_adapter(&id->adap);
 	if (ret < 0) {
 		dev_err(&pdev->dev, "reg adap failed: %d\n", ret);
 		goto err_clk_dis;
