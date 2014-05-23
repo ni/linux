8e73dbdedfc3e43396466ff3465fcaab9cee377c
Karthik Manamcheri <karthik.manamcheri@ni.com>
2014-05-23 10:00:22 -0500

    8250: NI 16550 can have different FIFO sizes
    The NI 16550 is a CPLD UART and depending on restrictions on CPLD size,
    different implementations can have different FIFO sizes. This change
    provides two NI 16550 types, one with a smaller 16-byte FIFOs and the
    other with the original 128-byte FIFOs.
    
    Signed-off-by: Karthik Manamcheri <karthik.manamcheri@ni.com>
    Acked-by: Justin Tang <justin.tang@ni.com>
    Acked-by: Jaeden Amero <jaeden.amero@ni.com>
    [bstreiff: renumber PORT_NI16550_*]
    Signed-off-by: Brandon Streiff <brandon.streiff@ni.com>

--- include/uapi/linux/serial_core.h
+++ include/uapi/linux/serial_core.h
@@ -294,6 +294,7 @@
 #define PORT_LINFLEXUART	122
 
 /* National Instruments 16550 UART */
-#define PORT_NI16550	123
+#define PORT_NI16550_F16	123 /* 16-byte FIFOs */
+#define PORT_NI16550_F128	124 /* 128-byte FIFOs */
 
 #endif /* _UAPILINUX_SERIAL_CORE_H */
