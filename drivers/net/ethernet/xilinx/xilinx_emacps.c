2b8e45549690560cf236976d4185d105225b10c2
Xander Huff <xander.huff@ni.com>
2014-11-03 13:42:02 -0600

    fixup: xemacps: Ensure we unmap bad DMA buffers
    This is a fixup to 1c145c7c5c298252f3d663a9e6ac8b7bd023fa69 and should
    be applied at first opportunity.
    
    After mapping DMA buffers, we calculate the length and compare to the
    maximum buffer size and the minimum Ethernet packet length. If this
    calculated length falls outside the range, we skip the packet.
    However, we need to make sure we also unmap the buffer we've already
    mapped before we skip it.
    
    Signed-off-by: Xander Huff <xander.huff@ni.com>
    Acked-by: Ben Shelton <ben.shelton@ni.com>
    Acked-by: Jaeden Amero <jaeden.amero@ni.com>
    ---
    Natinst-ReviewBoard-ID: 82259

--- drivers/net/ethernet/xilinx/xilinx_emacps.c
+++ drivers/net/ethernet/xilinx/xilinx_emacps.c
@@ -1393,19 +1393,19 @@ static int xemacps_rx(struct net_local *lp, int budget)
 			break;
 		}
 
-		/* the packet length */
-		len = ctrl & XEMACPS_RXBUF_LEN_MASK;
-
-		/* Break if the length is too small or big (drop) */
-		if (len < ETH_ZLEN || len > XEMACPS_RX_BUF_SIZE)
-			goto rx_skip;
-
 		rmb();
 		skb = lp->rx_skb[lp->rx_bd_ci].skb;
 		dma_unmap_single(lp->ndev->dev.parent,
 				lp->rx_skb[lp->rx_bd_ci].mapping,
 				lp->rx_skb[lp->rx_bd_ci].len,
 				DMA_FROM_DEVICE);
+
+		/* the packet length */
+		len = ctrl & XEMACPS_RXBUF_LEN_MASK;
+
+		/* Break if the length is too small or big (drop) */
+		if (len < ETH_ZLEN || len > XEMACPS_RX_BUF_SIZE)
+			goto rx_skip;
 
 		/* setup received skb and send it upstream */
 		skb_put(skb, len);  /* Tell the skb how much data we got. */
