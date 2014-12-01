de702d2cec858b59d4bff40102c5af2ebd96a12b
Jaeden Amero <jaeden.amero@ni.com>
2014-12-01 10:57:41 -0600

    sdhci-of-arasan: Follow mmc bindings
    The documentation for the OF bindings for sdhci-of-arasan
    (Documentation/devicetree/bindings/mmc/arasan,sdhci.txt) stated that the
    bindings follow the mmc bindings. However, mmc_of_parse was never called
    and thus those bindings were never actually followed.
    
    It is useful to be able to follow the mmc bindings, so let's do that.
    
    In following the mmc bindings, we have to supply our own sdhci op
    "get_max_clock", otherwise, the sdhci driver will overwrite the
    mmc_of_parse-supplied value for 'max-frequency'. It is desirable to
    specify a 'max-frequency' (for digital signal integrity reasons), so
    we'll use the 'max-frequency' value if non-zero, and the maximum of the
    underlying clock otherwise.
    
    Signed-off-by: Jaeden Amero <jaeden.amero@ni.com>
    Acked-by: Ben Shelton <ben.shelton@ni.com>
    Acked-by: Brad Mouring <brad.mouring@ni.com>
    Acked-by: Gratian Crisan <gratian.crisan@ni.com>
    Natinst-ReviewBoard-ID: 84053

--- drivers/mmc/host/sdhci-of-arasan.c
+++ drivers/mmc/host/sdhci-of-arasan.c
@@ -36,6 +36,20 @@ struct sdhci_arasan_data {
 	struct clk	*clk_ahb;
 };
 
+static unsigned int sdhci_arasan_get_max_clock(struct sdhci_host *host)
+{
+	unsigned int max_clock;
+
+	/* Make sure we can listen to the mmc 'max-frequency' parameter, which
+	 * gets set in host->mmc->f_max in mmc_of_parse. */
+	if (host->mmc->f_max)
+		max_clock = host->mmc->f_max;
+	else
+		max_clock = sdhci_pltfm_clk_get_max_clock(host);
+
+	return max_clock;
+}
+
 static unsigned int sdhci_arasan_get_timeout_clock(struct sdhci_host *host)
 {
 	u32 div;
@@ -53,7 +67,7 @@ static unsigned int sdhci_arasan_get_timeout_clock(struct sdhci_host *host)
 
 static struct sdhci_ops sdhci_arasan_ops = {
 	.set_clock = sdhci_set_clock,
-	.get_max_clock = sdhci_pltfm_clk_get_max_clock,
+	.get_max_clock = sdhci_arasan_get_max_clock,
 	.get_timeout_clock = sdhci_arasan_get_timeout_clock,
 	.set_bus_width = sdhci_set_bus_width,
 	.reset = sdhci_reset,
