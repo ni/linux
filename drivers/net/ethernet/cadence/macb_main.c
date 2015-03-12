9d2e5a4a507aab2222cd4d43db989af8c5fd00d6
Mihai Neagu <mihai.neagu@ni.com>
2015-03-12 18:53:00 +0200

    HACK net/macb: Totally disable TX checksum offloading
    Since hardware checksumming is not supported, reset the TXCOEN bit in
    DMACFG. If unitialized, this bit causes checksum offloading to be left
    on in hardware, with software thinking it is off, and for a checksum
    offloading bug in hardware to cause small UDP packets (<= 2 bytes) to be
    sent with an incorrect checksum (0xffff).
    
    We can't fix this problem by zeroing the checksum field in outgoing
    UDPv4 packets as implemented by Jeff Westfahl or Xilinx in their xemacps
    driver because it breaks packet forwarding (where it is illegal to
    modify the checksum of forwarded packets with unknown validity). We
    don't yet know a way to fix this problem other than to not use hardware
    checksum offloading.
    
    This commit will need to be reworked before it is upstreamable. We just
    need a fix in the LabVIEW RT Linux kernel right now for release.
    Ideally, we'd continue to work with upstream to solve the problem, but
    this is an emergency and we need to test and ship right away.
    
    Signed-off-by: Mihai Neagu <mihai.neagu@ni.com>
    Signed-off-by: Jaeden Amero <jaeden.amero@ni.com>
    Reviewed-by: Ben Shelton <ben.shelton@ni.com>
    Reviewed-by: Josh Cartwright <joshc@ni.com>
    Natinst-CAR-ID: 516373
    Natinst-ReviewBoard-ID: 97988
    (joshc: also hack up hw_features to never advertise hw csum support)
    Signed-off-by: Josh Cartwright <joshc@ni.com>

--- drivers/net/ethernet/cadence/macb_main.c
+++ drivers/net/ethernet/cadence/macb_main.c
@@ -2025,7 +2025,7 @@ static void macb_configure_dma(struct macb *bp)
 		else
 			dmacfg |= GEM_BIT(ENDIA_DESC); /* CPU in big endian */
 
-		if (bp->dev->features & NETIF_F_HW_CSUM)
+		if (0 && bp->dev->features & NETIF_F_HW_CSUM)
 			dmacfg |= GEM_BIT(TXCOEN);
 		else
 			dmacfg &= ~GEM_BIT(TXCOEN);
@@ -3046,7 +3046,7 @@ static int macb_init(struct platform_device *pdev)
 		dev->hw_features |= MACB_NETIF_LSO;
 
 	/* Checksum offload is only available on gem with packet buffer */
-	if (macb_is_gem(bp) && !(bp->caps & MACB_CAPS_FIFO_MODE))
+	if (0 && macb_is_gem(bp) && !(bp->caps & MACB_CAPS_FIFO_MODE))
 		dev->hw_features |= NETIF_F_HW_CSUM | NETIF_F_RXCSUM;
 	if (bp->caps & MACB_CAPS_SG_DISABLED)
 		dev->hw_features &= ~NETIF_F_SG;
