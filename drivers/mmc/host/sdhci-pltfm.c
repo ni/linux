b16d594b9a21e5a69029beea3fd0376e7a4e9cd2
Chen Yee Chew <chen.yee.chew@ni.com>
2015-05-28 21:11:02 +0800

    HACK sd: Prevent SD from doing high-speed timing
    This will prevent SD card from doing SD high-speed timing. It will not do
    SD high-speed timing for high-speed or standard-speed card.
    
    Signed-off-by: Chen Yee Chew <chen.yee.chew@ni.com>
    Reviewed-by: Keng Soon Cheah <keng.soon.cheah@ni.com>
    Reviewed-by: Joe Hershberger <joe.hershberger@ni.com>
    Natinst-CAR-ID: 519438
    Natinst-ReviewBoard-ID: 99036

--- drivers/mmc/host/sdhci-pltfm.c
+++ drivers/mmc/host/sdhci-pltfm.c
@@ -93,6 +93,9 @@ void sdhci_get_of_property(struct platform_device *pdev)
 		host->quirks2 |=
 			SDHCI_QUIRK2_MUST_FORCE_SD_CD_TEST_MODE;
 
+	if (of_get_property(np, "force-sd-standard", NULL))
+		 host->quirks |= SDHCI_QUIRK_NO_HISPD_BIT;
+
 	if (of_get_property(np, "no-1-8-v", NULL))
 		host->quirks2 |= SDHCI_QUIRK2_NO_1_8_V;
 
