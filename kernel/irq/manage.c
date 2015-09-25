206a8e87bd9834cd7555ba8505ed84047afa1f1f
Haris Okanovic <haris.okanovic@ni.com>
2015-09-25 11:34:42 -0500

    proc/interrupts: Add polling
    Implement polling on procfs' "interrupts" file which observes changes
    to IRQ action handlers. The poll fires each time an action handler is
    registered or unregistered.
    
    This change enables daemons to watch for changes and apply certain
    system policies relating to IRQ processing. For example, modify
    execution priority of dedicated IRQ tasks after they're created.
    
    include/linux/interrupt.h
    kernel/irq/manage.c
     Add change counter for handler registrations and a wait queue to notify
     tasks on updates.
    
    fs/proc/interrupts.c
     Add polling callback on aforementioned counter and wait queue.
    
    Signed-off-by: Haris Okanovic <haris.okanovic@ni.com>
    Signed-off-by: Ovidiu-Adrian Vancea <ovidiu.vancea@ni.com>
    Signed-off-by: Brad Mouring <brad.mouring@ni.com>
    Natinst-ReviewBoard-ID: 111860, 163902
    [gratian: fixed small rebase conflict]
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>
    [bstreiff: un-trivialize from changes in fddda2b7b521 ("proc: introduce
     proc_create_seq{,_data}") and convert file_operations to proc_ops]
    Signed-off-by: Brandon Streiff <brandon.streiff@ni.com>

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -35,6 +35,22 @@ static int __init setup_forced_irqthreads(char *arg)
 early_param("threadirqs", setup_forced_irqthreads);
 #endif
 
+static atomic_long_t irq_handler_change_count = ATOMIC_LONG_INIT(0);
+DECLARE_WAIT_QUEUE_HEAD(irq_handler_change_wq);
+
+/* Bump change count and wake up anything waiting on changes to
+ * IRQ handlers */
+static void __irq_handler_change_event(void)
+{
+	atomic_long_inc(&irq_handler_change_count);
+	wake_up(&irq_handler_change_wq);
+}
+
+long get_irq_handler_change_count(void)
+{
+	return atomic_long_read(&irq_handler_change_count);
+}
+
 static void __synchronize_hardirq(struct irq_desc *desc, bool sync_chip)
 {
 	struct irq_data *irqd = irq_desc_get_irq_data(desc);
@@ -1818,6 +1834,7 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 	register_irq_proc(irq, desc);
 	new->dir = NULL;
 	register_handler_proc(irq, new);
+	__irq_handler_change_event();
 	return 0;
 
 mismatch:
@@ -1996,6 +2013,7 @@ static struct irqaction *__free_irq(struct irq_desc *desc, void *dev_id)
 	irq_chip_pm_put(&desc->irq_data);
 	module_put(desc->owner);
 	kfree(action->secondary);
+	__irq_handler_change_event();
 	return action;
 }
 
