11938e770b3750c2820e789fc68ddd7cdcf67c35
Haris Okanovic <haris.okanovic@ni.com>
2015-09-25 11:34:42 -0500

    proc/interrupts: Add polling
    Implement polling on procfs' "interrupts" file which observes changes
    to IRQ action handlers. The poll fires each time an action handler is
    registered or unregistered.
    
    This change enables daemons to watch for changes and apply certain
    system policies relating to IRQ processing. For example, modify
    execution priority of dedicated IRQ tasks after they're created.
    
    include/linux/interrupt.h
    kernel/irq/manage.c
     Add change counter for handler registrations and a wait queue to notify
     tasks on updates.
    
    fs/proc/interrupts.c
     Add polling callback on aforementioned counter and wait queue.
    
    Signed-off-by: Haris Okanovic <haris.okanovic@ni.com>
    Signed-off-by: Ovidiu-Adrian Vancea <ovidiu.vancea@ni.com>
    Signed-off-by: Brad Mouring <brad.mouring@ni.com>
    Natinst-ReviewBoard-ID: 111860, 163902
    [gratian: fixed small rebase conflict]
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>
    [bstreiff: un-trivialize from changes in fddda2b7b521 ("proc: introduce
     proc_create_seq{,_data}") and convert file_operations to proc_ops]
    Signed-off-by: Brandon Streiff <brandon.streiff@ni.com>

--- fs/proc/interrupts.c
+++ fs/proc/interrupts.c
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/fs.h>
+#include <linux/poll.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/irqnr.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <linux/slab.h>
 
 /*
  * /proc/interrupts
@@ -34,9 +36,79 @@ static const struct seq_operations int_seq_ops = {
 	.show  = show_interrupts
 };
 
+struct interrupts_fd_state {
+	atomic_long_t last_irq_change_count;
+};
+
+static int interrupts_open(struct inode *inode, struct file *filp)
+{
+	int res;
+	struct interrupts_fd_state *privdata;
+	struct seq_file *sf;
+
+	privdata = kzalloc(sizeof(struct interrupts_fd_state), GFP_KERNEL);
+	if (!privdata) {
+		res = -ENOMEM;
+		goto exit;
+	}
+
+	res = seq_open(filp, &int_seq_ops);
+	if (res) {
+		kfree(privdata);
+		goto exit;
+	}
+
+	sf = filp->private_data;
+	sf->private = privdata;
+
+	atomic_long_set(&(privdata->last_irq_change_count),
+		get_irq_handler_change_count());
+
+exit:
+	return res;
+}
+
+static int interrupts_release(struct inode *inode, struct file *filp)
+{
+	struct seq_file *sf = filp->private_data;
+
+	kfree(sf->private);
+	return seq_release(inode, filp);
+}
+
+static unsigned int interrupts_poll(struct file *filp,
+	struct poll_table_struct *pt)
+{
+	unsigned int mask = POLLIN | POLLRDNORM;
+	long newcount, oldcount;
+	struct seq_file *sf = filp->private_data;
+	struct interrupts_fd_state *fds = sf->private;
+
+	/* Register for changes to IRQ handlers */
+	poll_wait(filp, &irq_handler_change_wq, pt);
+
+	/* Store new change count in priv data */
+	newcount = get_irq_handler_change_count();
+	oldcount = atomic_long_xchg(
+		&(fds->last_irq_change_count), newcount);
+
+	if (newcount != oldcount)
+		mask |= POLLERR | POLLPRI;
+
+	return mask;
+}
+
+static const struct proc_ops interrupts_proc_ops = {
+	.proc_open	= interrupts_open,
+	.proc_read	= seq_read,
+	.proc_poll	= interrupts_poll,
+	.proc_lseek	= seq_lseek,
+	.proc_release	= interrupts_release,
+};
+
 static int __init proc_interrupts_init(void)
 {
-	proc_create_seq("interrupts", 0, NULL, &int_seq_ops);
+	proc_create("interrupts", 0, NULL, &interrupts_proc_ops);
 	return 0;
 }
 fs_initcall(proc_interrupts_init);
