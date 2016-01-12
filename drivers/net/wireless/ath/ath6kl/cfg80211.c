d01dd02dd950409d6b342acc75b56ea951a631f4
Brad Mouring <brad.mouring@ni.com>
2016-01-12 16:06:56 -0600

    Revert "ath6kl: Disable AP mode for Silex firmware"
    This reverts commit 52774c503fa2fe5d5a4aa95c89014f3bca6da638.

--- drivers/net/wireless/ath/ath6kl/cfg80211.c
+++ drivers/net/wireless/ath/ath6kl/cfg80211.c
@@ -3730,10 +3730,8 @@ int ath6kl_cfg80211_init(struct ath6kl *ar)
 	set_wiphy_dev(wiphy, ar->dev);
 
 	wiphy->interface_modes = BIT(NL80211_IFTYPE_STATION) |
-				 BIT(NL80211_IFTYPE_ADHOC);
-#ifndef CONFIG_ATH6KL_SILEX_FIRMWARE
-	wiphy->interface_modes |= BIT(NL80211_IFTYPE_AP);
-#endif
+				 BIT(NL80211_IFTYPE_ADHOC) |
+				 BIT(NL80211_IFTYPE_AP);
 	if (ar->p2p) {
 		wiphy->interface_modes |= BIT(NL80211_IFTYPE_P2P_GO) |
 					  BIT(NL80211_IFTYPE_P2P_CLIENT);
