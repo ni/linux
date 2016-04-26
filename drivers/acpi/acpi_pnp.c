62aa39d763a77a3b0a4b3d771424935587879594
Nathan Sullivan <nathan.sullivan@ni.com>
2016-04-26 15:59:36 -0500

    ACPI / PNP: Add IDs for NI 16550 to list
    Add the two PNP IDs for NI 16550 serial ports to the PNP device ID
    list, so they are enumerated as PNP devices.
    
    Signed-off-by: Nathan Sullivan <nathan.sullivan@ni.com>
    Signed-off-by: Brad Mouring <brad.mouring@ni.com>
    Acked-by: Xander Huff <xander.huff@ni.com>
    
    Natinst-ReviewBoard-ID: 135498
    Natinst-CAR-ID: 584586

--- drivers/acpi/acpi_pnp.c
+++ drivers/acpi/acpi_pnp.c
@@ -218,6 +218,8 @@ static const struct acpi_device_id acpi_pnp_device_ids[] = {
 	{"MVX00A1"},		/*  Deskline K56 Phone System PnP */
 	{"MVX00F2"},		/* PC Rider K56 Phone System PnP */
 	{"nEC8241"},		/* NEC 98NOTE SPEAKER PHONE FAX MODEM(33600bps) */
+	{"NIC7750"},		/* National Instruments (NI) 16550 PNP */
+	{"NIC7772"},		/* National Instruments (NI) 16550 PNP */
 	{"PMC2430"},		/* Pace 56 Voice Internal Plug & Play Modem */
 	{"PNP0500"},		/* Generic standard PC COM port     */
 	{"PNP0501"},		/* Generic 16550A-compatible COM port */
