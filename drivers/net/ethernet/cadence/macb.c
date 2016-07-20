92440cfb1266567def487d17afba43545942db3b
Brad Mouring <brad.mouring@ni.com>
2016-07-20 13:04:11 -0500

    HACK: net: macb: Trigger phy state machine on _open()
    d5c3d8465 (net: phy: Avoid polling PHY with PHY_IGNORE_INTERRUPTS)
    changed the behavior of the phy state machine to only be triggered
    on interrupts.
    
    Previously, the state machine work would re-arm itself regardless
    of whether the phydev supported interrupts.
    
    With the state machine being run to update state only on phy irqs
    when dealing with an irqful phy, if that phy were to interrupt early
    (before the macb ndev is open()ed) or only after entering aneg,
    the phy will remain in UP as there's nothing to run phy_state_machine
    (it either already fired interrupts or the phy is waiting to enter
    ANEG to make changes that will fire interrupts)
    
    For a more general solution, force the state machine to rerun
    after starting the mac (moving the phy state machine from READY
    to UP, with nothing to move into AN).
    
    Signed-off-by: Brad Mouring <brad.mouring@ni.com>
    Acked-by: Josh Cartwright <joshc@ni.com>
    
    Natinst-ReviewBoard-ID: 148264
    (xhuff: modified to work with 0a91281e5190)
    Signed-off-by: Xander Huff <xander.huff@ni.com>
    Natinst-ReviewBoard-ID: 156344

--- drivers/net/ethernet/cadence/macb.c
+++ drivers/net/ethernet/cadence/macb.c
@@ -1999,6 +1999,7 @@ static int macb_open(struct net_device *dev)
 {
 	struct macb *bp = netdev_priv(dev);
 	struct macb_queue *queue;
+	struct phy_device *phydev = dev->phydev;
 	unsigned int q;
 	size_t bufsz = dev->mtu + ETH_HLEN + ETH_FCS_LEN + NET_IP_ALIGN;
 	int err;
@@ -2018,7 +2019,7 @@ static int macb_open(struct net_device *dev)
 	netif_carrier_off(dev);
 
 	/* if the phy is not yet register, retry later*/
-	if (!dev->phydev)
+	if (!phydev)
 		return -EAGAIN;
 
 	/* RX buffers initialization */
@@ -2058,7 +2059,9 @@ done_irq:
 	macb_init_hw(bp);
 
 	/* schedule a link state check */
-	phy_start(dev->phydev);
+	phy_start(phydev);
+	cancel_delayed_work_sync(&phydev->state_queue);
+	queue_delayed_work(system_power_efficient_wq, &phydev->state_queue, 0);
 
 	netif_tx_start_all_queues(dev);
 
