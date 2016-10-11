d5393f4711d518dbf181fc71e80e386c793da775
Xander Huff <xander.huff@ni.com>
2016-10-11 14:54:57 -0500

    Revert "HACK: net: macb: Trigger phy state machine on _open()"
    This reverts:
      commit 9f7aed53b891 ("HACK: net: macb: Trigger phy state machine on
      _open()")
    
    Signed-off-by: Xander Huff <xander.huff@ni.com>
    Natinst-ReviewBoard-ID: 156344

--- drivers/net/ethernet/cadence/macb.c
+++ drivers/net/ethernet/cadence/macb.c
@@ -1970,7 +1970,6 @@ static int macb_open(struct net_device *dev)
 {
 	struct macb *bp = netdev_priv(dev);
 	struct macb_queue *queue;
-	struct phy_device *phydev = bp->phy_dev;
 	unsigned int q;
 	size_t bufsz = dev->mtu + ETH_HLEN + ETH_FCS_LEN + NET_IP_ALIGN;
 	int err;
@@ -1990,7 +1989,7 @@ static int macb_open(struct net_device *dev)
 	netif_carrier_off(dev);
 
 	/* if the phy is not yet register, retry later*/
-	if (!phydev)
+	if (!bp->phy_dev)
 		return -EAGAIN;
 
 	/* RX buffers initialization */
@@ -2030,9 +2029,7 @@ done_irq:
 	macb_init_hw(bp);
 
 	/* schedule a link state check */
-	phy_start(phydev);
-	cancel_delayed_work_sync(&phydev->state_queue);
-	queue_delayed_work(system_power_efficient_wq, &phydev->state_queue,0);
+	phy_start(bp->phy_dev);
 
 	netif_tx_start_all_queues(dev);
 
