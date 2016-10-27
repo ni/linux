ni/linux

commit acd55a2db7613b68c0f7fa4b9d79d887a653ccd1

8250: Handle FPGA reprogramming
The 8250 driver will now subscribe to and handle fpgaperipheral
notifications. When the FPGA disappears, the port be suspended, but
not removed from the system. When the FPGA reappears, the port will be
resumed.

Signed-off-by: Jaeden Amero <jaeden.amero@ni.com>

drivers/tty/serial/8250/8250_core.c
@@ -43,6 +43,10 @@
 
 #include <asm/irq.h>
 
+#ifdef CONFIG_FPGA_PERIPHERAL
+#include <misc/fpgaperipheral.h>
+#endif
+
 #include "8250.h"
 
 /*
@@ -957,6 +961,157 @@ static struct uart_8250_port *serial8250_find_match_or_unused(struct uart_port *
 	return NULL;
 }
 
+#ifdef CONFIG_FPGA_PERIPHERAL
+static unsigned int disabled_serial_in(struct uart_port *p, int offset)
+{
+	WARN(1, "FPGA Access Error: Cannot read from UART for ttyS%d",
+	     p->line);
+	return 0;
+}
+
+static void disabled_serial_out(struct uart_port *p, int offset, int value)
+{
+	WARN(1, "FPGA Access Error: Cannot write to UART for ttyS%d",
+	     p->line);
+}
+
+static int serial8250_fpga_notification(struct notifier_block *nb,
+					unsigned long val, void *v)
+{
+	int ret = -EINVAL;
+	struct uart_port *port = container_of(nb, struct uart_port, nb);
+
+	pr_debug("ttyS%d - serial8250_fpga_notification, val=%lu\n",
+		 serial_index(port), val);
+
+	switch (val) {
+	case FPGA_PERIPHERAL_DOWN:
+		/* If the FPGA goes down for reprogramming, we do the
+		 * following steps:
+		 *
+		 * 1. Suspend the port. This would shutdown the port, unlink
+		 *    the irqs and set the suspended flag. It is not guaranteed
+		 *    that hardware will not be accessed when the port is
+		 *    suspended. Some functions do not honor port suspension
+		 *    and access the FPGA anyways.
+		 */
+		ret = uart_suspend_port(&serial8250_reg, port);
+
+		/*
+		 * 2. Grab the fpga_lock which would prevent other functions
+		 *    from accessing the hardware. (All hardware accessing
+		 *    functions should grab this lock before doing anything)
+		 *
+		 * Note: This lock should NOT be grabbed before we call
+		 * uart_suspend_port since the suspend code path also grabs
+		 * this lock. If we have the lock before we call
+		 * uart_suspend_port, we will deadlock the system.
+		 */
+
+		down_write(&port->fpga_lock);
+
+		/*
+		 * 3. Set the FPGA state to FPGA_DOWN.
+		 */
+		port->fpga_state = FPGA_DOWN;
+
+		/*
+		 * 4. As a failsafe, change the serial_in and serial_out
+		 *    functions to empty functions thereby making sure that
+		 *    we never access the hardware
+		 */
+		port->serial_in = disabled_serial_in;
+		port->serial_out = disabled_serial_out;
+
+		break;
+
+	case FPGA_PERIPHERAL_UP:
+		/* If the FGPA programming succeeds and the FGPA is back
+		 * online, we do the following steps:
+		 *
+		 * 1. We short-circuited the serial_in and serial_out functions
+		 *    as a failsafe. Here we restore the original functions.
+		 */
+		serial8250_set_defaults(up_to_u8250p(port));
+
+		/*
+		 * 2. Set the FPGA state to FPGA_UP
+		 */
+		port->fpga_state = FPGA_UP;
+
+		/*
+		 * 3. Unlock the FPGA lock. This will let other functions
+		 *    which were waiting for the FPGA to run. These other
+		 *    functions should check for the fpga_state before
+		 *    continuing.
+		 *
+		 * Note: We SHOULD release the lock before calling
+		 * uart_resume_port since the resume code path relies on this
+		 * lock being released. If we don't release the lock before
+		 * resuming the port, we will deadlock the system.
+		 */
+		up_write(&port->fpga_lock);
+
+		/*
+		 * 4. Resume the port. This is the complementary function to
+		 *    uart_suspend_port. We will relink the irqs, unset
+		 *    the suspended flag and start transfers (if any).
+		 */
+		ret = uart_resume_port(&serial8250_reg, port);
+
+		break;
+
+	case FPGA_PERIPHERAL_FAILED:
+		dev_err(port->dev, "ttyS%d - FPGA programming failed.\n",
+			serial_index(port));
+		/*
+		 * If the FPGA programming fails for some reason, we do the
+		 * following steps:
+		 *
+		 * 1. Set the FPGA state to FPGA_FAILED
+		 */
+		port->fpga_state = FPGA_FAILED;
+
+		/*
+		 * 2. Unlock the FPGA lock. The functions which were waiting
+		 *    for the FGPA can now check for fpga_state and return
+		 *    errors because of FPGA programming failure.
+		 */
+		up_write(&port->fpga_lock);
+
+		break;
+
+	default:
+		pr_err("ttyS%d - Unknown FPGA notification value encountered\n",
+		       serial_index(port));
+	}
+
+	if (ret < 0) {
+		pr_err("ttyS%d - FPGA notification error %d\n",
+		       serial_index(port), ret);
+	}
+
+	/* We ignore all errors encountered in this function in order to ensure
+	 * that the fpgaperipheral blocking chain notifier notifies all
+	 * clients. (See documentation in include/misc/fpgaperipheral.h) */
+	return notifier_from_errno(0);
+}
+
+int serial8250_setup_fpga_notifier(struct uart_port *port)
+{
+	init_rwsem(&port->fpga_lock);
+
+	port->nb.notifier_call = serial8250_fpga_notification;
+	return blocking_notifier_chain_register(&fpgaperipheral_notifier_list,
+						&port->nb);
+}
+#else
+static inline int serial8250_setup_fpga_notifier(struct uart_port *port)
+{
+	return 0;
+}
+#endif
+
 /**
  *	serial8250_register_8250_port - register a serial port
  *	@up: serial port template
@@ -1053,6 +1208,13 @@ int serial8250_register_8250_port(struct uart_8250_port *up)
 				serial8250_isa_config(0, &uart->port,
 						&uart->capabilities);
 
+			ret = serial8250_setup_fpga_notifier(&uart->port);
+			if (ret < 0) {
+				dev_err(uart->port.dev,
+					"notify register failed; err=%i\n", ret);
+				goto unlock;
+			}
+
 			ret = uart_add_one_port(&serial8250_reg,
 						&uart->port);
 			if (ret == 0)
@@ -1067,6 +1229,8 @@ int serial8250_register_8250_port(struct uart_8250_port *up)
 			ret = 0;
 		}
 	}
+unlock:
+
 	mutex_unlock(&serial_mutex);
 
 	return ret;

include/linux/serial_core.h
@@ -25,6 +25,7 @@
 #include <linux/interrupt.h>
 #include <linux/circ_buf.h>
 #include <linux/spinlock.h>
+#include <linux/rwsem.h>
 #include <linux/sched.h>
 #include <linux/tty.h>
 #include <linux/mutex.h>
@@ -36,6 +37,10 @@
 	((port)->cons && (port)->cons->index == (port)->line)
 #else
 #define uart_console(port)      ({ (void)port; 0; })
+#endif
+
+#ifdef CONFIG_FPGA_PERIPHERAL
+#include <linux/notifier.h>
 #endif
 
 struct uart_port;
@@ -151,6 +156,23 @@ struct uart_port {
 	unsigned char		regshift;		/* reg offset shift */
 	unsigned char		iotype;			/* io access style */
 	unsigned char		unused1;
+
+#ifdef CONFIG_FPGA_PERIPHERAL
+	struct notifier_block	nb;
+	unsigned int		fpga_state;
+#define FPGA_UP			(0)
+#define FPGA_DOWN		(1)
+#define FPGA_FAILED		(2)
+
+	/*
+	 * Functions which need to access the UART should acquire a
+	 * read lock on this. The FPGA will not get reprogrammed unless this
+	 * lock is released. The FGPA reprogramming will acquire an
+	 * exclusive lock (write lock) on this, thereby preventing the said
+	 * functions from accessing the UART in the FPGA
+	 */
+	struct rw_semaphore	fpga_lock;
+#endif
 
 #define UPIO_PORT		(SERIAL_IO_PORT)	/* 8b I/O port access */
 #define UPIO_HUB6		(SERIAL_IO_HUB6)	/* Hub6 ISA card */

drivers/tty/serial/serial_core.c
@@ -95,7 +95,7 @@ static void __uart_start(struct tty_struct *tty)
 	struct uart_state *state = tty->driver_data;
 	struct uart_port *port = state->uart_port;
 
-	if (!uart_tx_stopped(port))
+	if (!uart_tx_stopped(port) && !port->suspended)
 		port->ops->start_tx(port);
 }
 
@@ -160,6 +160,14 @@ static int uart_port_startup(struct tty_struct *tty, struct uart_state *state,
 		uart_circ_clear(&state->xmit);
 	}
 
+#ifdef CONFIG_FPGA_PERIPHERAL
+	down_read(&uport->fpga_lock);
+	if (uport->fpga_state != FPGA_UP) {
+		up_read(&uport->fpga_lock);
+		return -ENODEV;
+	}
+#endif
+
 	retval = uport->ops->startup(uport);
 	if (retval == 0) {
 		if (uart_console(uport) && uport->cons->cflag) {
@@ -190,6 +198,10 @@ static int uart_port_startup(struct tty_struct *tty, struct uart_state *state,
 	 */
 	if (retval && capable(CAP_SYS_ADMIN))
 		return 1;
+
+#ifdef CONFIG_FPGA_PERIPHERAL
+	up_read(&uport->fpga_lock);
+#endif
 
 	return retval;
 }
@@ -972,6 +984,14 @@ static int uart_tiocmget(struct tty_struct *tty)
 	struct uart_port *uport = state->uart_port;
 	int result = -EIO;
 
+#ifdef CONFIG_FPGA_PERIPHERAL
+	down_read(&uport->fpga_lock);
+	if (uport->fpga_state != FPGA_UP) {
+		up_read(&uport->fpga_lock);
+		return -ENODEV;
+	}
+#endif
+
 	mutex_lock(&port->mutex);
 	if (!(tty->flags & (1 << TTY_IO_ERROR))) {
 		result = uport->mctrl;
@@ -980,6 +1000,10 @@ static int uart_tiocmget(struct tty_struct *tty)
 		spin_unlock_irq(&uport->lock);
 	}
 	mutex_unlock(&port->mutex);
+
+#ifdef CONFIG_FPGA_PERIPHERAL
+	up_read(&uport->fpga_lock);
+#endif
 
 	return result;
 }
@@ -992,12 +1016,25 @@ uart_tiocmset(struct tty_struct *tty, unsigned int set, unsigned int clear)
 	struct tty_port *port = &state->port;
 	int ret = -EIO;
 
+#ifdef CONFIG_FPGA_PERIPHERAL
+	down_read(&uport->fpga_lock);
+	if (uport->fpga_state != FPGA_UP) {
+		up_read(&uport->fpga_lock);
+		return -ENODEV;
+	}
+#endif
+
 	mutex_lock(&port->mutex);
 	if (!(tty->flags & (1 << TTY_IO_ERROR))) {
 		uart_update_mctrl(uport, set, clear);
 		ret = 0;
 	}
 	mutex_unlock(&port->mutex);
+
+#ifdef CONFIG_FPGA_PERIPHERAL
+	up_read(&uport->fpga_lock);
+#endif
+
 	return ret;
 }
 
@@ -1211,6 +1248,9 @@ uart_ioctl(struct tty_struct *tty, unsigned int cmd,
 	struct tty_port *port = &state->port;
 	void __user *uarg = (void __user *)arg;
 	int ret = -ENOIOCTLCMD;
+#ifdef CONFIG_FPGA_PERIPHERAL
+	struct uart_port *uport = state->uart_port;
+#endif
 
 
 	/*
@@ -1250,6 +1290,15 @@ uart_ioctl(struct tty_struct *tty, unsigned int cmd,
 	/*
 	 * The following should only be used when hardware is present.
 	 */
+
+#ifdef CONFIG_FPGA_PERIPHERAL
+	down_read(&uport->fpga_lock);
+	if (uport->fpga_state != FPGA_UP) {
+		up_read(&uport->fpga_lock);
+		return -ENODEV;
+	}
+#endif
+
 	switch (cmd) {
 	case TIOCMIWAIT:
 		ret = uart_wait_modem_status(state, arg);
@@ -1293,6 +1342,9 @@ uart_ioctl(struct tty_struct *tty, unsigned int cmd,
 out_up:
 	mutex_unlock(&port->mutex);
 out:
+#ifdef CONFIG_FPGA_PERIPHERAL
+	up_read(&uport->fpga_lock);
+#endif
 	return ret;
 }
 
@@ -1342,6 +1394,18 @@ static void uart_set_termios(struct tty_struct *tty,
 		return;
 	}
 
+#ifdef CONFIG_FPGA_PERIPHERAL
+	down_read(&uport->fpga_lock);
+	/*
+	 * If the FPGA state is not FPGA_UP (the re-programming failed),
+	 * we cannot return an error code from this function.
+	 */
+	if (uport->fpga_state != FPGA_UP) {
+		up_read(&uport->fpga_lock);
+		return;
+	}
+#endif
+
 	mutex_lock(&state->port.mutex);
 	uart_change_speed(tty, state, old_termios);
 	mutex_unlock(&state->port.mutex);
@@ -1358,6 +1422,10 @@ static void uart_set_termios(struct tty_struct *tty,
 			mask |= TIOCM_RTS;
 		uart_set_mctrl(uport, mask);
 	}
+
+#ifdef CONFIG_FPGA_PERIPHERAL
+	up_read(&uport->fpga_lock);
+#endif
 }
 
 /*
@@ -1482,7 +1550,7 @@ static void uart_wait_until_sent(struct tty_struct *tty, int timeout)
 	 * 'timeout' / 'expire' give us the maximum amount of time
 	 * we wait.
 	 */
-	while (!port->ops->tx_empty(port)) {
+	while (port->suspended || !port->ops->tx_empty(port)) {
 		msleep_interruptible(jiffies_to_msecs(char_time));
 		if (signal_pending(current))
 			break;

commit 89b0704804fbdda67b9db29498cd310a8e37fbe5

8250: Replace 'FGPA' with 'FPGA'
Fixes: acd55a2db761 ("8250: Handle FPGA reprogramming")
Signed-off-by: Xander Huff <xander.huff@ni.com>
Signed-off-by: Brad Mouring <brad.mouring@ni.com>
Natinst-ReviewBoard-ID: 156851

drivers/tty/serial/8250/8250_core.c
@@ -1026,7 +1026,7 @@ static int serial8250_fpga_notification(struct notifier_block *nb,
 		break;
 
 	case FPGA_PERIPHERAL_UP:
-		/* If the FGPA programming succeeds and the FGPA is back
+		/* If the FPGA programming succeeds and the FPGA is back
 		 * online, we do the following steps:
 		 *
 		 * 1. We short-circuited the serial_in and serial_out functions
@@ -1074,7 +1074,7 @@ static int serial8250_fpga_notification(struct notifier_block *nb,
 
 		/*
 		 * 2. Unlock the FPGA lock. The functions which were waiting
-		 *    for the FGPA can now check for fpga_state and return
+		 *    for the FPGA can now check for fpga_state and return
 		 *    errors because of FPGA programming failure.
 		 */
 		up_write(&port->fpga_lock);

include/linux/serial_core.h
@@ -167,7 +167,7 @@ struct uart_port {
 	/*
 	 * Functions which need to access the UART should acquire a
 	 * read lock on this. The FPGA will not get reprogrammed unless this
-	 * lock is released. The FGPA reprogramming will acquire an
+	 * lock is released. The FPGA reprogramming will acquire an
 	 * exclusive lock (write lock) on this, thereby preventing the said
 	 * functions from accessing the UART in the FPGA
 	 */