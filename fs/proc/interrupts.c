ni/linux

commit 8e2f148e2f4762cc2b6490ec01d5d31bc440bcf8

proc/interrupts: Add polling
Implement polling on procfs' "interrupts" file which observes changes
to IRQ action handlers. The poll fires each time an action handler is
registered or unregistered.

This change enables daemons to watch for changes and apply certain
system policies relating to IRQ processing. For example, modify
execution priority of dedicated IRQ tasks after they're created.

include/linux/interrupt.h
kernel/irq/manage.c
 Add change counter for handler registrations and a wait queue to notify
 tasks on updates.

fs/proc/interrupts.c
 Add polling callback on aforementioned counter and wait queue.

Signed-off-by: Haris Okanovic <haris.okanovic@ni.com>
Signed-off-by: Ovidiu-Adrian Vancea <ovidiu.vancea@ni.com>
Signed-off-by: Brad Mouring <brad.mouring@ni.com>
Natinst-ReviewBoard-ID: 111860, 163902

fs/proc/interrupts.c
@@ -1,9 +1,11 @@
 #include <linux/fs.h>
+#include <linux/poll.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/irqnr.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <linux/slab.h>
 
 /*
  * /proc/interrupts
@@ -33,16 +35,74 @@ static const struct seq_operations int_seq_ops = {
 	.show  = show_interrupts
 };
 
+struct interrupts_fd_state {
+	atomic_long_t last_irq_change_count;
+};
+
 static int interrupts_open(struct inode *inode, struct file *filp)
 {
-	return seq_open(filp, &int_seq_ops);
+	int res;
+	struct interrupts_fd_state *privdata;
+	struct seq_file *sf;
+
+	privdata = kzalloc(sizeof(struct interrupts_fd_state), GFP_KERNEL);
+	if (!privdata) {
+		res = -ENOMEM;
+		goto exit;
+	}
+
+	res = seq_open(filp, &int_seq_ops);
+	if (res) {
+		kfree(privdata);
+		goto exit;
+	}
+
+	sf = filp->private_data;
+	sf->private = privdata;
+
+	atomic_long_set(&(privdata->last_irq_change_count),
+		get_irq_handler_change_count());
+
+exit:
+	return res;
+}
+
+static int interrupts_release(struct inode *inode, struct file *filp)
+{
+	struct seq_file *sf = filp->private_data;
+
+	kfree(sf->private);
+	return seq_release(inode, filp);
+}
+
+static unsigned int interrupts_poll(struct file *filp,
+	struct poll_table_struct *pt)
+{
+	unsigned int mask = 0;
+	long newcount, oldcount;
+	struct seq_file *sf = filp->private_data;
+	struct interrupts_fd_state *fds = sf->private;
+
+	/* Register for changes to IRQ handlers */
+	poll_wait(filp, &irq_handler_change_wq, pt);
+
+	/* Store new change count in priv data */
+	newcount = get_irq_handler_change_count();
+	oldcount = atomic_long_xchg(
+		&(fds->last_irq_change_count), newcount);
+
+	if (newcount != oldcount)
+		mask = POLLIN | POLLRDNORM;
+
+	return mask;
 }
 
 static const struct file_operations proc_interrupts_operations = {
 	.open		= interrupts_open,
 	.read		= seq_read,
+	.poll		= interrupts_poll,
 	.llseek		= seq_lseek,
-	.release	= seq_release,
+	.release	= interrupts_release,
 };
 
 static int __init proc_interrupts_init(void)

include/linux/interrupt.h
@@ -738,4 +738,7 @@ extern char __softirqentry_text_end[];
 #define __softirq_entry
 #endif
 
+extern long get_irq_handler_change_count(void);
+extern wait_queue_head_t irq_handler_change_wq;
+
 #endif

kernel/irq/manage.c
@@ -41,6 +41,22 @@ void init_irq_default_prio(struct irq_desc *desc)
 	desc->irq_data.priority = irqthread_pri;
 }
 
+static atomic_long_t irq_handler_change_count = ATOMIC_LONG_INIT(0);
+DECLARE_WAIT_QUEUE_HEAD(irq_handler_change_wq);
+
+/* Bump change count and wake up anything waiting on changes to
+ * IRQ handlers */
+static void __irq_handler_change_event(void)
+{
+	atomic_long_inc(&irq_handler_change_count);
+	wake_up(&irq_handler_change_wq);
+}
+
+long get_irq_handler_change_count(void)
+{
+	return atomic_long_read(&irq_handler_change_count);
+}
+
 static void __synchronize_hardirq(struct irq_desc *desc)
 {
 	bool inprogress;
@@ -1526,7 +1542,7 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 	new->dir = NULL;
 	register_handler_proc(irq, new);
 	free_cpumask_var(mask);
-
+	__irq_handler_change_event();
 	return 0;
 
 mismatch:
@@ -1676,6 +1692,7 @@ static struct irqaction *__free_irq(unsigned int irq, void *dev_id)
 
 	module_put(desc->owner);
 	kfree(action->secondary);
+	__irq_handler_change_event();
 	return action;
 }
 

commit 0052fbe093de56130ce59288ea739382f64b84d1

fixup! proc/interrupts: Add polling
Be consistent between poll() implementations, like
commit 31b07093c44a ("proc: mounts_poll() make consistent to mdstat_poll").

Fixes: 8e2f148e2f (proc/interrupts: Add polling)
Signed-off-by: Ovidiu-Adrian Vancea <ovidiu.vancea@ni.com>
Signed-off-by: Brad Mouring <brad.mouring@ni.com>
Acked-by: Haris Okanovic <haris.okanovic@ni.com>
Acked-by: Julia Cartwright <julia@ni.com>
Natinst-ReviewBoard-ID: 169152

fs/proc/interrupts.c
@@ -78,7 +78,7 @@ static int interrupts_release(struct inode *inode, struct file *filp)
 static unsigned int interrupts_poll(struct file *filp,
 	struct poll_table_struct *pt)
 {
-	unsigned int mask = 0;
+	unsigned int mask = POLLIN | POLLRDNORM;
 	long newcount, oldcount;
 	struct seq_file *sf = filp->private_data;
 	struct interrupts_fd_state *fds = sf->private;
@@ -92,7 +92,7 @@ static unsigned int interrupts_poll(struct file *filp,
 		&(fds->last_irq_change_count), newcount);
 
 	if (newcount != oldcount)
-		mask = POLLIN | POLLRDNORM;
+		mask |= POLLERR | POLLPRI;
 
 	return mask;
 }