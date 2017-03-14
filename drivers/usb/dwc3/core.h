426f4a958948621e2ad29e5028cbe58ecbc6d173
Wan Ahmad Zainie <wan.ahmad.zainie.wan.mohamad@intel.com>
2017-03-14 15:45:21 -0500

    usb: dwc3: call _DSM for core soft reset
    The issue is, if core soft reset is issued while Intel Apollo Lake
    USB mux is in Host role mode, it takes close to 7 minutes before we
    are able to switch USB mux from Host mode to Device mode. This is
    due to RTL bug.
    
    The workaround is to let BIOS issue the core soft reset via _DSM
    method. It will ensure that USB mux is in Device role mode before
    issuing core soft reset, and will inform the driver whether the
    reset is success within the timeout value, or the timeout is exceeded.
    
    commit cd78b8067c6e ("usb: dwc3: call _DSM for core soft reset")
    originated from http://git.yoctoproject.org/cgit/cgit.cgi/linux-yocto-4.1/
    
    Signed-off-by: Wan Ahmad Zainie <wan.ahmad.zainie.wan.mohamad@intel.com>
    [akash.mankar@ni.com: changed the way has_dsm_for_softreset property is
    set in dwc3-pci.c and read in core.c]
    Signed-off-by: Akash Mankar <akash.mankar@ni.com>
    Signed-off-by: Brad Mouring <brad.mouring@ni.com>
    Acked-by: Gratian Crisan <gratian.crisan@ni.com>
    Acked-by: Brandon Streiff <brandon.streif@ni.com>
    Natinst-ReviewBoard-ID: 178124
    [gratian: fixed merge conflicts, mainly due to dwc3_soft_reset removal]
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>
    [bstreiff: fixed merge conflicts due to property refactor by 1a7b12f69a94
     ("usb: dwc3: pci: Supply device properties via driver data")]
    [gratian: fix merge conflict with f580170f135a
     ("usb: dwc3: Add splitdisable quirk for Hisilicon Kirin Soc")]
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>
    [gratian: fix conflict with 582ab24e096f
     ("usb: dwc3: pci: Set "linux,phy_charger_detect" property on some Bay Trail boards")]
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>
    [cvadrevu: fix trivial conflict with 047161686b813
     ("usb: dwc3: Add remote wakeup handling")]
    [cvadrevu: fix trivial conflict with 4e8ef34e36f28
     ("usb: dwc3: fix gadget mode suspend interrupt handler issue")]
    [cvadrevu: fix conflict with 8bea147dfdf82
     ("usb: dwc3: Soft reset phy on probe for host")]
    [cvadrevu: fix conflict with 917dc99b65911
     ("usb: dwc3: pci: Change PCI device macros")]
    Signed-off-by: Chaitanya Vadrevu <chaitanya.vadrevu@ni.com>
    [cvadrevu: fix conflict with 4fad737008679
     ("usb: dwc3: core: Fix system suspend on TI AM62 platforms")]
    [cvadrevu: fix conflict with e8d48c2282a91
     ("Revert "usb: dwc3: Soft reset phy on probe for host"")]
    Signed-off-by: Chaitanya Vadrevu <chaitanya.vadrevu@emerson.com>

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -1157,6 +1157,7 @@ struct dwc3_scratchpad_array {
  * @suspended: set to track suspend event due to U3/L2.
  * @susphy_state: state of DWC3_GUSB2PHYCFG_SUSPHY + DWC3_GUSB3PIPECTL_SUSPHY
  *		  before PM suspend.
+ * @has_dsm_for_softreset: set if we want to use BIOS to do core soft reset
  * @imod_interval: set the interrupt moderation interval in 250ns
  *			increments or 0 to disable.
  * @max_cfg_eps: current max number of IN eps used across all USB configs.
@@ -1390,6 +1391,8 @@ struct dwc3 {
 	unsigned		wakeup_configured:1;
 	unsigned		suspended:1;
 	unsigned		susphy_state:1;
+
+	unsigned		has_dsm_for_softreset:1;
 
 	u16			imod_interval;
 
