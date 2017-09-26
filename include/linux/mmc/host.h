3293218e2506888fcb35213f4bd42e898d6e3379
Kyle Roeschley <kyle.roeschley@ni.com>
2017-09-26 14:57:06 -0500

    mmc: core: Add capability for 1.8V-only controllers
    On some SD host controllers, 1.8V signaling is required for reliable
    operation of the part. Add a capability which says that we must switch
    to 1.8V signaling during card initialization.
    
    Signed-off-by: Kyle Roeschley <kyle.roeschley@ni.com>
    Signed-off-by: Brad Mouring <brad.mouring@ni.com>
    Acked-by: James Minor <james.minor@ni.com>
    Acked-by: Brandon Streiff <brandon.streiff@ni.com>
    Natinst-ReviewBoard-ID: 203804
    [gratian: fix small conflict caused by af51715079e7 ("MMC core learns about SPI")]
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -349,6 +349,7 @@ struct mmc_host {
 #define MMC_CAP2_NO_MMC		(1 << 22)	/* Do not send (e)MMC commands during initialization */
 #define MMC_CAP2_CQE		(1 << 23)	/* Has eMMC command queue engine */
 #define MMC_CAP2_CQE_DCMD	(1 << 24)	/* CQE can issue a direct command */
+#define MMC_CAP2_REQUIRE_1_8V	(1 << 25)	/* Host requires 1.8V signalling */
 
 	mmc_pm_flag_t		pm_caps;	/* supported pm features */
 
