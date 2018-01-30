b2cb8187def79d8bd1a6c776c394c32c59d56f28
Nathan Sullivan <nathan.sullivan@ni.com>
2018-01-30 13:42:20 -0600

    macb: deactivate queues in fpga notifier
    In order to synchronize macb_stop and macb_start_xmit, turn all the tx
    queues off before going down and back on again before going up during
    FPGA reloads.  Not synchronizing these functions would result in a crash
    during the reload if frames are transmitted.
    
    Signed-off-by: Nathan Sullivan <nathan.sullivan@ni.com>
    Signed-off-by: Brad Mouring <brad.mouring@ni.com>
    Acked-by: Zach Brown <zach.brown@ni.com>
    Acked-by: James Minor <james.minor@ni.com>
    Natinst-ReviewBoard-ID: 218609
    Natinst-CAR-ID: 672398

--- drivers/net/ethernet/cadence/macb_main.c
+++ drivers/net/ethernet/cadence/macb_main.c
@@ -38,6 +38,7 @@
 
 #ifdef CONFIG_FPGA_PERIPHERAL
 #include <linux/rtnetlink.h>
+#include <net/sch_generic.h>
 #include <misc/fpgaperipheral.h>
 #endif
 
@@ -2836,6 +2837,7 @@ int macb_fpga_notifier(struct notifier_block *nb, unsigned long val, void *data)
 		if (!bp->fpga_down) {
 			/* If the interface has been opened. */
 			if (netif_running(bp->dev)) {
+				dev_deactivate(bp->dev);
 				macb_close(bp->dev);
 				phy_stop_interrupts(dev->phydev);
 				phy_stop_machine_nolink(dev->phydev);
@@ -2862,6 +2864,7 @@ int macb_fpga_notifier(struct notifier_block *nb, unsigned long val, void *data)
 			phy_start_machine(dev->phydev);
 			phy_start_interrupts(dev->phydev);
 			macb_open(bp->dev);
+			dev_activate(bp->dev);
 		}
 
 		rtnl_unlock();
