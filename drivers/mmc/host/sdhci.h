5ad6479db85134a86e80469a04a0dfe3a6f303ce
Kyle Roeschley <kyle.roeschley@ni.com>
2018-05-16 16:45:01 -0500

    mmc: sdhci: Add quirk work around double rescan
    On some SD host controllers, we can see two SD card insert interrupts when
    inserting a card. This causes mmc_rescan() to be called twice in quick
    succession and generate one or two SD card clock pulses, which can cause
    some SD cards to become unresponsive.
    
    Signed-off-by: Kyle Roeschley <kyle.roeschley@ni.com>
    Signed-off-by: Brad Mouring <brad.mouring@ni.com>
    Acked-by: Tony Liechty <tony.liechty@ni.com>
    Acked-by: Nathan Sullivan <nathan.sullivan@ni.com>
    Natinst-ReviewBoard-ID: 236135
    [gratian: fix conflict with fec796739740 ("mmc: sdhci: Factor out sdhci_enable_clk")]
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>
    [bstreiff: minor change to renumber flag bit]
    Signed-off-by: Brandon Streiff <brandon.streiff@ni.com>
    [gratian: minor change to renumber flag bit]
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -482,6 +482,8 @@ struct sdhci_host {
 #define SDHCI_QUIRK2_ISSUE_CMD_DAT_RESET_TOGETHER	(1<<19)
 /* Controller requires delay between card clock disable and param change */
 #define SDHCI_QUIRK2_NEED_DELAY_AFTER_CLK_DISABLE	(1<<20)
+/* Controller may interrupt multiple times for card insert */
+#define SDHCI_QUIRK2_SPURIOUS_CARD_INSERT_INTERRUPT	(1<<21)
 
 	int irq;		/* Device IRQ */
 	void __iomem *ioaddr;	/* Mapped address */
