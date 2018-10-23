ni/linux

commit 2012c036bf2853761ffc275c15568ba1356b490d

usb: dwc3: call _DSM for core soft reset
The issue is, if core soft reset is issued while Intel Apollo Lake
USB mux is in Host role mode, it takes close to 7 minutes before we
are able to switch USB mux from Host mode to Device mode. This is
due to RTL bug.

The workaround is to let BIOS issue the core soft reset via _DSM
method. It will ensure that USB mux is in Device role mode before
issuing core soft reset, and will inform the driver whether the
reset is success within the timeout value, or the timeout is exceeded.

commit cd78b8067c6e ("usb: dwc3: call _DSM for core soft reset")
originated from http://git.yoctoproject.org/cgit/cgit.cgi/linux-yocto-4.1/

Signed-off-by: Wan Ahmad Zainie <wan.ahmad.zainie.wan.mohamad@intel.com>
[akash.mankar@ni.com: changed the way has_dsm_for_softreset property is
set in dwc3-pci.c and read in core.c]
Signed-off-by: Akash Mankar <akash.mankar@ni.com>
Signed-off-by: Brad Mouring <brad.mouring@ni.com>
Acked-by: Gratian Crisan <gratian.crisan@ni.com>
Acked-by: Brandon Streiff <brandon.streif@ni.com>
Natinst-ReviewBoard-ID: 178124
[gratian: fixed merge conflicts, mainly due to dwc3_soft_reset removal]
Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>

drivers/usb/dwc3/dwc3-pci.c
@@ -105,6 +105,17 @@ static int dwc3_pci_quirks(struct dwc3_pci *dwc)
 		return platform_device_add_properties(dwc3, properties);
 	}
 
+	if (pdev->vendor == PCI_VENDOR_ID_INTEL &&
+	    (pdev->device == PCI_DEVICE_ID_INTEL_BXT ||
+	     pdev->device == PCI_DEVICE_ID_INTEL_BXT_M ||
+	     pdev->device == PCI_DEVICE_ID_INTEL_APL)) {
+		struct property_entry properties[] = {
+			PROPERTY_ENTRY_BOOL("snps,has_dsm_for_softreset"),
+			{ }
+		};
+		return platform_device_add_properties(dwc3, properties);
+	}
+
 	if (pdev->vendor == PCI_VENDOR_ID_INTEL) {
 		int ret;
 

drivers/usb/dwc3/core.c
@@ -207,6 +207,38 @@ u32 dwc3_core_fifo_space(struct dwc3_ep *dep, u8 type)
 }
 
 /**
+ * WORKAROUND: We let BIOS issues the core soft reset to Device
+ * controller for Intel Apollo Lake, via _DSM method.
+ *
+ * The issue is, if core soft reset is issued while Intel Apollo Lake
+ * USB mux is in Host role mode, it takes close to 7 minutes before
+ * we are able to switch USB mux from Host mode to Device mode.
+ */
+static int dwc3_pci_dsm_soft_reset(struct device *dev)
+{
+	int			ret = -ETIMEDOUT;
+	union acpi_object	*obj;
+	guid_t			guid;
+
+	WARN_ON(guid_parse("732b85d5-b7a7-4a1b-9ba0-4bbd00ffd511", &guid));
+
+	obj = acpi_evaluate_dsm(ACPI_HANDLE(dev),
+				&guid,
+				1, 6, NULL);
+	if (!obj) {
+		dev_err(dev, "failed to evaluate _DSM\n");
+		return -EIO;
+	}
+
+	if (obj->type == ACPI_TYPE_INTEGER)
+		ret = (obj->integer.value == 0) ? 0 : -ETIMEDOUT;
+	dev_dbg(dev, "dwc3_pci_dsm_soft_reset() ret= %d\n", ret);
+
+	ACPI_FREE(obj);
+	return ret;
+}
+
+/**
  * dwc3_core_soft_reset - Issues core soft reset and PHY reset
  * @dwc: pointer to our context structure
  */
@@ -235,6 +267,11 @@ static int dwc3_core_soft_reset(struct dwc3 *dwc)
 	 */
 	if (dwc->dr_mode == USB_DR_MODE_HOST)
 		return 0;
+
+	if (dwc->has_dsm_for_softreset) {
+		dev_dbg(dwc->dev, "calling dwc3_pci_dsm_soft_reset()");
+		return dwc3_pci_dsm_soft_reset(dwc->dev);
+	}
 
 	reg = dwc3_readl(dwc->regs, DWC3_DCTL);
 	reg |= DWC3_DCTL_CSFTRST;
@@ -1088,6 +1125,9 @@ static void dwc3_get_properties(struct dwc3 *dwc)
 				    &dwc->hsphy_interface);
 	device_property_read_u32(dev, "snps,quirk-frame-length-adjustment",
 				 &dwc->fladj);
+
+	dwc->has_dsm_for_softreset = device_property_read_bool(dev,
+				"snps,has_dsm_for_softreset");
 
 	dwc->lpm_nyet_threshold = lpm_nyet_threshold;
 	dwc->tx_de_emphasis = tx_de_emphasis;

drivers/usb/dwc3/core.h
@@ -867,6 +867,7 @@ struct dwc3_scratchpad_array {
  * 	1	- -3.5dB de-emphasis
  * 	2	- No de-emphasis
  * 	3	- Reserved
+ * @has_dsm_for_softreset: set if we want to use BIOS to do core soft reset
  * @imod_interval: set the interrupt moderation interval in 250ns
  *                 increments or 0 to disable.
  */
@@ -1019,6 +1020,8 @@ struct dwc3 {
 
 	unsigned		tx_de_emphasis_quirk:1;
 	unsigned		tx_de_emphasis:2;
+
+	unsigned		has_dsm_for_softreset:1;
 
 	u16			imod_interval;
 };

commit cbafa276aae7ce0c63adfbb024b676d14aac63af

usb: dwc3: pci: Fix property init for Apollo Lake
The initialization of device properties for Apollo Lake and Broxton CPUs was
bailing out early which prevented the definition of sysdev_is_parent and
dr_mode properties.

The missing sysdev_is_parent property made a call to dma_alloc_coherent() fail
when a USB gadget driver was loaded.

The missing dr_mode property lead to a non-fatal dmesg error.

To fix this, ensure that the sysdev_is_parent and dr_mode property is defined
for Apollo Lake and Broxton CPUs.

Fixes: 2012c036bf28 ("usb: dwc3: call _DSM for core soft reset")
Signed-off-by: Ken Sharp <ken.sharp@ni.com>
Acked-by: Kyle Roeschley <kyle.roeschley@ni.com>
Acked-by: Gratian Crisan <gratian.crisan@ni.com>
Natinst-CAR-ID: 715260
Natinst-ReviewBoard-ID: 259661

drivers/usb/dwc3/dwc3-pci.c
@@ -105,27 +105,27 @@ static int dwc3_pci_quirks(struct dwc3_pci *dwc)
 		return platform_device_add_properties(dwc3, properties);
 	}
 
-	if (pdev->vendor == PCI_VENDOR_ID_INTEL &&
-	    (pdev->device == PCI_DEVICE_ID_INTEL_BXT ||
-	     pdev->device == PCI_DEVICE_ID_INTEL_BXT_M ||
-	     pdev->device == PCI_DEVICE_ID_INTEL_APL)) {
-		struct property_entry properties[] = {
-			PROPERTY_ENTRY_BOOL("snps,has_dsm_for_softreset"),
-			{ }
-		};
-		return platform_device_add_properties(dwc3, properties);
-	}
-
 	if (pdev->vendor == PCI_VENDOR_ID_INTEL) {
 		int ret;
 
-		struct property_entry properties[] = {
-			PROPERTY_ENTRY_STRING("dr_mode", "peripheral"),
-			PROPERTY_ENTRY_BOOL("linux,sysdev_is_parent"),
-			{ }
-		};
-
-		ret = platform_device_add_properties(dwc3, properties);
+		if (pdev->device == PCI_DEVICE_ID_INTEL_BXT ||
+		    pdev->device == PCI_DEVICE_ID_INTEL_BXT_M ||
+		    pdev->device == PCI_DEVICE_ID_INTEL_APL) {
+			struct property_entry properties[] = {
+				PROPERTY_ENTRY_BOOL("snps,has_dsm_for_softreset"),
+				PROPERTY_ENTRY_STRING("dr_mode", "peripheral"),
+				PROPERTY_ENTRY_BOOL("linux,sysdev_is_parent"),
+				{ }
+			};
+			ret =  platform_device_add_properties(dwc3, properties);
+		} else {
+			struct property_entry properties[] = {
+				PROPERTY_ENTRY_STRING("dr_mode", "peripheral"),
+				PROPERTY_ENTRY_BOOL("linux,sysdev_is_parent"),
+				{ }
+			};
+			ret = platform_device_add_properties(dwc3, properties);
+		}
 		if (ret < 0)
 			return ret;
 