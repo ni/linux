c27883c546ddef3c2e3311aacb4e63db8dcb129d
John Ogness <john.ogness@linutronix.de>
2020-11-30 01:42:04 +0106

    printk: call boot_delay_msec() in printk_delay()
    boot_delay_msec() is always called immediately before printk_delay()
    so just call it from within printk_delay().
    
    Signed-off-by: John Ogness <john.ogness@linutronix.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -1755,8 +1755,10 @@ SYSCALL_DEFINE3(syslog, int, type, char __user *, buf, int, len)
 
 int printk_delay_msec __read_mostly;
 
-static inline void printk_delay(void)
+static inline void printk_delay(int level)
 {
+	boot_delay_msec(level);
+
 	if (unlikely(printk_delay_msec)) {
 		int m = printk_delay_msec;
 
@@ -2226,8 +2228,7 @@ asmlinkage int vprintk_emit(int facility, int level,
 		in_sched = true;
 	}
 
-	boot_delay_msec(level);
-	printk_delay();
+	printk_delay(level);
 
 	printed_len = vprintk_store(facility, level, dev_info, fmt, args);
 
