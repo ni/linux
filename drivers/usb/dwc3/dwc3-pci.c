fdb5b10037c1cfb1a348e7a55d9017e82053069b
Chaitanya Vadrevu <chaitanya.vadrevu@ni.com>
2022-08-25 14:26:49 -0500

    Revert "usb: dwc3: Fix NULL pointer exception in dwc3_pci_remove()"
    This reverts commit 7b412b04a0c7000293008231ce8413056abb1982.
    
    Signed-off-by: Chaitanya Vadrevu <chaitanya.vadrevu@ni.com>

--- drivers/usb/dwc3/dwc3-pci.c
+++ drivers/usb/dwc3/dwc3-pci.c
@@ -348,10 +348,8 @@ err:
 static void dwc3_pci_remove(struct pci_dev *pci)
 {
 	struct dwc3_pci		*dwc = pci_get_drvdata(pci);
-	struct pci_dev		*pdev = dwc->pci;
 
-	if (pdev->device == PCI_DEVICE_ID_INTEL_BYT)
-		gpiod_remove_lookup_table(&platform_bytcr_gpios);
+	gpiod_remove_lookup_table(&platform_bytcr_gpios);
 #ifdef CONFIG_PM
 	cancel_work_sync(&dwc->wakeup_work);
 #endif
