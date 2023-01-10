78eec117bbf41d92fb8e888bafd0cdd978a47d7f
Charlie Johnston <charlie.johnston@ni.com>
2023-01-10 16:00:42 -0600

    Revert "e1000e: Add delays after writing to registers"
    This reverts commit c689a30f629739a68f351013f4885fbfa8703880.
    
    The custom delay patch for the E1000 driver no longer
    appears necessary based on hand-testing.
    
    Signed-off-by: Charlie Johnston <charlie.johnston@ni.com>

--- drivers/net/ethernet/intel/e1000/e1000_main.c
+++ drivers/net/ethernet/intel/e1000/e1000_main.c
@@ -2158,7 +2158,6 @@ static void e1000_enter_82542_rst(struct e1000_adapter *adapter)
 	rctl = er32(RCTL);
 	rctl |= E1000_RCTL_RST;
 	ew32(RCTL, rctl);
-	E1000_WR_DELAY();
 	E1000_WRITE_FLUSH();
 	mdelay(5);
 
@@ -2175,7 +2174,6 @@ static void e1000_leave_82542_rst(struct e1000_adapter *adapter)
 	rctl = er32(RCTL);
 	rctl &= ~E1000_RCTL_RST;
 	ew32(RCTL, rctl);
-	E1000_WR_DELAY();
 	E1000_WRITE_FLUSH();
 	mdelay(5);
 
@@ -2324,9 +2322,6 @@ static void e1000_set_rx_mode(struct net_device *netdev)
 		 */
 		E1000_WRITE_REG_ARRAY(hw, MTA, i, mcarray[i]);
 	}
-
-	E1000_WR_DELAY();
-
 	E1000_WRITE_FLUSH();
 
 	if (hw->mac_type == e1000_82542_rev2_0)
