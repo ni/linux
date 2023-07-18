706d00833c95012befb386cef663c52234e7b851
Charlie Johnston <charlie.johnston@ni.com>
2023-07-18 17:39:34 -0500

    Revert "mmc: sdhci: Add quirk for delay between clock disable and param change"
    This reverts commit 2e2158ac8d6bc9bbab7c4bfd61c0a67f0fde97c3.
    
    This change is no longer necessary as the original issue was not
    reproducible in 6.1 kernels.
    
    Signed-off-by: Charlie Johnston <charlie.johnston@ni.com>

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -481,8 +481,6 @@ struct sdhci_host {
 #define SDHCI_QUIRK2_USE_32BIT_BLK_CNT			(1<<18)
 /* Issue CMD and DATA reset together */
 #define SDHCI_QUIRK2_ISSUE_CMD_DAT_RESET_TOGETHER	(1<<19)
-/* Controller requires delay between card clock disable and param change */
-#define SDHCI_QUIRK2_NEED_DELAY_AFTER_CLK_DISABLE	(1<<19)
 
 	int irq;		/* Device IRQ */
 	void __iomem *ioaddr;	/* Mapped address */
